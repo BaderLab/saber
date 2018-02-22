c6924fb4922db17c340513fabe844aac2082bd1b
JohnGiorgi <johnmgiorgi@gmail.com>
2018-02-22 11:09:20 -0500

    Re-org of util scripts.
    Former-commit-id: 581e4f392d08cb9c0f2a731571f042145d0b56cb

---
+++ kari/utils_models.py
@@ -0,0 +1,24 @@
+from keras import optimizers
+
+""" A collection of model helper/utility functions. """
+
+def compile_model(learning_rate, optimizer, model, loss_function, metrics):
+    """ Compiles a model specified with Keras.
+
+    Args:
+        model_specifications: a dictionary containing model hyperparameters.
+        model: Keras model object to compile
+        loss_function: loss_function to compile model with
+        metrics: Keras metrics to compile model with
+    """
+    ## COMPILE
+    if optimizer == 'sgd':
+        optimizer_ = optimizers.SGD(lr=learning_rate)
+    # It is recommended to leave the parameters of this optimizer at their
+    # default values (except the learning rate, which can be freely tuned).
+    # This optimizer is usually a good choice for recurrent neural networks
+    elif optimizer == 'RMSprop':
+        optimizer_ = optimizers.RMSprop(lr=learning_rate)
+
+    model.compile(optimizer=optimizer_, loss=loss_function, metrics=[metrics])
+    model.summary()
