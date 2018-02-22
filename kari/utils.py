fdc528841f4a084a68cdf5d4030a468eacca05a8
JohnGiorgi <johnmgiorgi@gmail.com>
2018-02-21 20:25:45 -0500

    Adding utility function for creating directories.

--- kari/utils.py
+++ kari/utils.py
@@ -1,20 +1,18 @@
-# add back in POS later.
-class SentenceGetter(object):
+import os
+import errno
 
-    def __init__(self, data):
-        self.n_sent = 1
-        self.data = data
-        self.empty = False
-        agg_func = lambda s: [(w, p, t) for w, p, t in zip(s["Word"].values.tolist(),
-                                                           s["POS"].values.tolist(),
-                                                           s["Tag"].values.tolist())]
-        self.grouped = self.data.groupby("Sentence #").apply(agg_func)
-        self.sentences = [s for s in self.grouped]
+""" A collection of helper/utility functions. """
 
-    def get_next(self):
-        try:
-            s = self.grouped["Sentence: {}".format(self.n_sent)]
-            self.n_sent += 1
-            return s
-        except:
-            return None
+# https://stackoverflow.com/questions/273192/how-can-i-create-a-directory-if-it-does-not-exist#273227
+def make_dir(directory_filepath):
+    """ Creates a directory (directory_filepath) if it does not exist.
+
+    Args:
+        directory_filepath: filepath of directory to create
+    """
+    # create output directory if it does not exist
+    try:
+        os.makedirs(directory_filepath)
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise
