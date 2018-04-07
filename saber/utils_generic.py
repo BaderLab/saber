00d52935d677ef09daf759ddc1e2507322e2f0c1
johngiorgi <johnmgiorgi@gmail.com>
2018-04-07 10:29:55 -0400

    Removing requirement for sequence length
    Former-commit-id: 7d566e13eea45fcaa3a6a2bf738ede8e87acb3dd [formerly e2bc8eefba4acfcb5177af73a3a2d2c9363e70d1]
    Former-commit-id: 17c512df9060a1d302df243c3c52dabeb90aab60

--- saber/utils_generic.py
+++ saber/utils_generic.py
@@ -5,7 +5,7 @@ import errno
 
 # https://stackoverflow.com/questions/273192/how-can-i-create-a-directory-if-it-does-not-exist#273227
 def make_dir(directory_filepath):
-    """ Creates a directory (directory_filepath) if it does not exist.
+    """Creates a directory (directory_filepath) if it does not exist.
 
     Args:
         directory_filepath: filepath of directory to create
