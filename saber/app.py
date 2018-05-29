3b0be4363148239141f5e756449674404bff96b7
johngiorgi <johnmgiorgi@gmail.com>
2018-05-29 09:00:20 -0400

    Updating default entities to reflect new pre-trained models.

--- saber/app.py
+++ saber/app.py
@@ -1,22 +1,29 @@
+# -*- coding: utf-8 -*-
 import json
-import utils_web_service
+import utils_app
 
 from flask import request
 from flask import jsonify
 from flask import Flask
 app = Flask(__name__)
 
-ENTITIES = {'PRGE': True,
-            'LIVB': False,
-            'CHED': False,
+ENTITIES = {'ANAT': False,
+            'CHED': True,
             'DISO': False,
+            'LIVB': True,
+            'PRGE': True,
             'TRIG': False
             }
-MODELS = utils_web_service.load_models(ENTITIES)
+
+MODELS = utils_app.load_models(ENTITIES)
 
 @app.route('/annotate/text', methods=['POST'])
 def annotate_text():
-    """"""
+    """Annotates raw text recieved in a POST request.
+
+    Returns:
+        a json formatted string
+    """
     # force=True means Content-Type does not necc. have to be application/json
     # so long as json in POST request is valid.
     data = request.get_json(force=True)
@@ -27,16 +34,20 @@ def annotate_text():
     # decide which entities to annotate
     ents = ENTITIES
     if requested_ents is not None:
-        ents = utils_web_service.harmonize_entities(ents, requested_ents)
+        ents = utils_app.harmonize_entities(ents, requested_ents)
 
     annotation = predict(text, ents)
 
-    return annotation
-
+    return jsonify(annotation)
 
 @app.route('/annotate/pmid', methods=['POST'])
 def annotate_pmid():
-    """"""
+    """Annotates the abstract of the document with the given PubMed ID
+    recieved in a POST request.
+
+    Returns:
+        a json formatted string
+    """
     data = request.get_json(force=True)
     # get args from request json
     pmid = data['pmid']
@@ -45,17 +56,26 @@ def annotate_pmid():
     # decide which entities to annotate
     ents = ENTITIES
     if requested_ents is not None:
-        ents = utils_web_service.harmonize_entities(ents, requested_ents)
+        ents = utils_app.harmonize_entities(ents, requested_ents)
 
     # use Entrez Utilities Web Service API to get the abtract text
-    _, abstract = utils_web_service.get_pubmed_text(pmid)
+    _, abstract = utils_app.get_pubmed_text(pmid)
 
     annotation = predict(abstract, ents)
 
-    return annotation
+    return jsonify(annotation)
 
 def predict(text, ents):
-    """"""
+    """Annotates raw text for entities according to their boolean value in ents
+
+    Args:
+        text (str): raw text to be annotated
+        ents: dictionary of entity, boolean key, value pairs representing
+            whether or not to annotate the text for the given entities
+
+    Returns:
+        dict containing the annotated entities and processed text.
+    """
     annotations = []
 
     for k, v in ents.items():
@@ -66,13 +86,14 @@ def predict(text, ents):
         final_annotation = annotations[0]
     elif len(annotations) > 1:
         # load json strings as dicts and create combined entity list
-        combined_ents = sum([json.loads(ann)['ents'] for ann in annotations])
+        combined_ents = []
+        for ann in annotations:
+            combined_ents.extend(ann['ents'])
         # create json containing combined annotation
         final_annotation = annotations[0]
         final_annotation['ents'] = combined_ents
-        final_annotation = jsonify(final_annotation)
 
     return final_annotation
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host='0.0.0.0')
