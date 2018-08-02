3310e0f4c24237f48627d85aa4f7b883d8ba9b1d
johngiorgi <johnmgiorgi@gmail.com>
2018-08-02 14:40:56 -0400

    renaming saber.main to saber.train
    Former-commit-id: ee1023418bf2456c0b0f9722161ea2147e0c781f

---
+++ saber/train.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+import os
+
+from saber.config import Config
+from saber.sequence_processor import SequenceProcessor
+
+def main():
+    """Coordinates a complete training cycle, including reading in a config, loading dataset(s),
+    training the model, and saving the models weights."""
+    # create and collect model and training parameters
+    config = Config(cli=True)
+
+    # currently performs training by default
+    sp = SequenceProcessor(config)
+    sp.load_dataset()
+
+    # if pretrained token embeddings are provided, load them
+    if config.pretrained_embeddings is not None:
+        sp.load_embeddings()
+    sp.create_model()
+    sp.fit()
+
+    # save the model
+    if config.save_model:
+        ds_names = [os.path.basename(x) for x in config.dataset_folder]
+        saved_model_dir = os.path.join(config.output_folder, '_'.join(ds_names))
+        sp.save(saved_model_dir)
+
+if __name__ == '__main__':
+    main()
