76e39399e834217155087601f1899f5bd0596050
johngiorgi <johnmgiorgi@gmail.com>
2018-08-02 14:47:19 -0400

    Including help dialog in the CLI
    Former-commit-id: e542a860d4bb0e746eff7bf9b2bfe0dc0be9cd81
    Former-commit-id: 7c430a08caa0965baeab18591e755f11d12a07d5 [formerly dca85b4830b7c0e79f13a62bc71fe7ad1bf89a5b] [formerly 2da8c9a990da3b7504d759f02e56c86ba89ad183 [formerly 34c20da726c48a38181cc8ca92b93564fd46b7ba [formerly 72e922b7ad3a63d8018ab406e9cd71423f42bb09]]]
    Former-commit-id: 2871d6b4cad0a0449a28b7c5c19fb45afccc483d [formerly 906d3d77d6fabb00286ddb51a62089fe739ae4ea]
    Former-commit-id: 556ca8a9a55dab6f1c13c3ffa620dc191edac927
    Former-commit-id: 82b2f99da40aa0906e9a85bc8520fb97df3a8feb

--- saber/config.py
+++ saber/config.py
@@ -7,6 +7,8 @@ import configparser
 
 from .preprocessor import Preprocessor
 
+# TODO: Some arguments still need help strings written
+
 class Config(object):
     """A class for managing all hyperparameters and configurations of a model.
 
@@ -21,17 +23,15 @@ class Config(object):
     def __init__(self, filepath='./config.ini', cli=False):
         # filepath to config file
         self.filepath = os.path.join(os.path.dirname(os.path.abspath(__file__)), filepath)
-
         # a parsed configparser object
         self.config = None
 
+        # parse args provided in configuration file
+        self.config = self._parse_config_args(self.filepath)
         # parse cli arguments (if they exist)
-        self.cli_args = {}
-        if cli:
-            self._parse_cli_args()
-        self._parse_config_args(self.filepath)
+        self.cli_args = self._parse_cli_args() if cli else {}
         # harmonize cli and config arguments and apply post processing
-        self._process_args(self.cli_args)
+        self.args = self._process_args(self.cli_args)
 
     def _parse_config_args(self, filepath):
         """Returns a parsed configparser object for config file at 'filepath'.
@@ -40,14 +40,12 @@ class Config(object):
             filepath: filepath to .ini config file
 
         Returns:
-            True if config file at 'filepath' was parsed successfully.
+            ConfigParser object, parsed from .ini file at 'filepath'
         """
         config = configparser.ConfigParser()
         config.read(filepath)
 
-        self.config = config
-
-        return True
+        return config
 
     def _process_args(self, cli_args):
         """Collect arguments from ini file if specificed.
@@ -63,7 +61,7 @@ class Config(object):
                 command line (keys) and their values (values).
 
         Returns:
-            True if the method existed without error, false otherwise.
+            a dictionary containing the final harmonized and post processed arguments.
         """
         args = {}
 
@@ -71,60 +69,48 @@ class Config(object):
             # parse config
             # mode
             args['model_name'] = self.config['mode']['model_name']
-            args['train_model'] = self.config['mode'].getboolean \
-                ('train_model')
-            args['save_model'] = self.config['mode'].getboolean \
-                ('save_model')
-            args['load_pretrained_model'] = \
-                self.config['mode'].getboolean('load_pretrained_model')
+            args['train_model'] = self.config['mode'].getboolean('train_model')
+            args['save_model'] = self.config['mode'].getboolean('save_model')
+            args['load_pretrained_model'] = self.config['mode'].getboolean('load_pretrained_model')
 
             # data
-            args['dataset_folder'] = \
-                self.config['data']['dataset_folder'].split(',')
+            args['dataset_folder'] = self.config['data']['dataset_folder'].split(',')
             args['output_folder'] = self.config['data']['output_folder']
-            args['pretrained_model_weights'] = \
-                self.config['data']['pretrained_model_weights']
-            args['pretrained_embeddings'] = \
-                self.config['data']['pretrained_embeddings']
+            args['pretrained_model_weights'] = self.config['data']['pretrained_model_weights']
+            args['pretrained_embeddings'] = self.config['data']['pretrained_embeddings']
 
             # model
-            args['word_embed_dim'] = \
-                self.config['model'].getint('word_embed_dim')
-            args['char_embed_dim'] = \
-                self.config['model'].getint('char_embed_dim')
+            args['word_embed_dim'] = self.config['model'].getint('word_embed_dim')
+            args['char_embed_dim'] = self.config['model'].getint('char_embed_dim')
 
             # training
             args['optimizer'] = self.config['training']['optimizer']
-            args['activation'] = \
-                self.config['training']['activation']
-            args['learning_rate'] = \
-                self.config['training'].getfloat('learning_rate')
+            args['activation'] = self.config['training']['activation']
+            args['learning_rate'] = self.config['training'].getfloat('learning_rate')
             args['decay'] = self.config['training'].getfloat('decay')
-            args['grad_norm'] = \
-                self.config['training'].getfloat('grad_norm')
-            args['dropout_rate'] = \
-                self.config['training']['dropout_rate'].split(',')
-            args['batch_size'] = \
-                self.config['training'].getint('batch_size')
+            args['grad_norm'] = self.config['training'].getfloat('grad_norm')
+            args['dropout_rate'] = self.config['training']['dropout_rate'].split(',')
+            args['batch_size'] = self.config['training'].getint('batch_size')
             args['k_folds'] = self.config['training'].getint('k_folds')
             args['epochs'] = self.config['training'].getint('epochs')
             args['criteria'] = self.config['training']['criteria']
 
             # advanced
-            args['verbose'] = \
-                self.config['advanced'].getboolean('verbose')
+            args['verbose'] = self.config['advanced'].getboolean('verbose')
             args['debug'] = self.config['advanced'].getboolean('debug')
-            args['replace_rare_tokens'] = \
-                self.config['advanced'].getboolean('replace_rare_tokens')
+            args['tensorboard'] = self.config['advanced'].getboolean('tensorboard')
+            args['replace_rare_tokens'] = self.config['advanced'].getboolean('replace_rare_tokens')
             args['fine_tune_word_embeddings'] = \
                 self.config['advanced'].getboolean('fine_tune_word_embeddings')
+            # TEMP
+            args['variational_dropout'] = self.config['advanced'].getboolean('variational_dropout')
 
-        # Configparser throws a KeyError when you try to key into a configparser
-        # object that does not exist, catch it here, provide hint to the user
+        # Configparser throws a KeyError when you try to key into a configparser object that does
+        # not exist, catch it here, provide hint to the user
         except KeyError as key:
             print('[ERROR] KeyError raised for key {}.'.format(key))
-            print('[WARN] This likely happened because there is no .ini file at: {}'.format(self.filepath))
-
+            print(('[WARN] This may have happened because there is no .ini file at: '
+                   '{}'.format(self.filepath)))
         else:
             # overwrite any parameters in the config if specfied at CL
             for key, value in cli_args.items():
@@ -132,17 +118,15 @@ class Config(object):
                     args[key] = value
 
             # post-processing
-            args = self._post__process_args(args)
+            args = self._post_process_args(args)
 
             # use parameters dictionary to update instance attributes
-            for k, v in args.items():
-                setattr(self, k, v)
+            for arg, value in args.items():
+                setattr(self, arg, value)
 
-            return True
+        return args
 
-        return False
-
-    def _post__process_args(self, args):
+    def _post_process_args(self, args):
         """Post process parameters retrived from python config file.
 
         Performs series of post processing steps on 'parameters'. E.g., file and
@@ -155,12 +139,9 @@ class Config(object):
             args, where post-processing has been applied to some values.
         """
         # normalize strings
-        args['model_name'] = Preprocessor.sterilize(args['model_name'],
-                                                    lower=True)
-        args['optimizer'] = Preprocessor.sterilize(args['optimizer'],
-                                                   lower=True)
-        args['activation'] = \
-            Preprocessor.sterilize(args['activation'], lower=True)
+        args['model_name'] = Preprocessor.sterilize(args['model_name'], lower=True)
+        args['optimizer'] = Preprocessor.sterilize(args['optimizer'], lower=True)
+        args['activation'] = Preprocessor.sterilize(args['activation'], lower=True)
         args['criteria'] = Preprocessor.sterilize(args['criteria'], lower=True)
 
         # create normalized absolutized versions of paths
@@ -168,17 +149,12 @@ class Config(object):
                                   for ds in args['dataset_folder']]
         args['output_folder'] = os.path.abspath(args['output_folder'])
 
-        if args['pretrained_model_weights'] == '':
-            args['pretrained_model_weights'] = None
-        else:
-            args['pretrained_model_weights'] = \
-                os.path.abspath(args['pretrained_model_weights'])
+        # Normalize empty path arguments to None
+        args['pretrained_model_weights'] = None if args['pretrained_model_weights'] == '' \
+            else os.path.abspath(args['pretrained_model_weights'])
 
-        if args['pretrained_embeddings'] == '':
-            args['pretrained_embeddings'] = None
-        else:
-            args['pretrained_embeddings'] = \
-                os.path.abspath(args['pretrained_embeddings'])
+        args['pretrained_embeddings'] = None if args['pretrained_embeddings'] == '' \
+            else os.path.abspath(args['pretrained_embeddings'])
 
         # build dictionary for dropout rates
         args['dropout_rate'] = {
@@ -187,17 +163,13 @@ class Config(object):
             'recurrent': float(args['dropout_rate'][2]),
         }
 
-        # do not use gradient normalization if config value is 0
-        if args['grad_norm'] == 0:
-            args['grad_norm'] = None
-
         return args
 
     def _parse_cli_args(self):
         """Parse command line arguments passed with call to Saber.
 
         Returns:
-            True if command line arguments were parsed without error.
+            a dictionary containing all arguments and their values supplied at the command line.
         """
         parser = argparse.ArgumentParser(description='Saber CLI.')
 
@@ -208,8 +180,8 @@ class Config(object):
                             help=("Activation function to use in the dense layers. Defaults to "
                                   "'relu'."))
         parser.add_argument('--batch_size', required=False, type=int,
-                            help=('Integer or None. Number of samples per gradient update. Defaults '
-                                  'to 32.'))
+                            help=('Integer or None. Number of samples per gradient update.'
+                                  'Defaults to 32.'))
         parser.add_argument('--char_embed_dim', required=False, type=str,
                             help='Dimension of dense embeddings to be learned for each character.')
         parser.add_argument('--criteria', required=False, type=str,
@@ -218,18 +190,19 @@ class Config(object):
                                   "matching and 'exact' for exact-boundary matching."))
         parser.add_argument('--dataset_folder', required=False, nargs='*',
                             help=("Path to the dataset folder. Expects a file 'train.*' to be "
-                                  "present. Optionally, 'valid.*' and 'train.*' may be provided."))
+                                  "present. Optionally, 'valid.*' and 'train.*' may be provided."
+                                  "Multiple datasets can be provided, sperated by a space"))
         parser.add_argument('--debug', required=False, action='store_true',
                             help=('If provided, only a small proportion of the dataset, and any '
                                   'provided embeddings, are loaded. Useful for debugging.'))
         parser.add_argument('--decay', required=False, type=float,
                             help=('float >= 0. Learning rate decay over each update. Note that for '
                                   'certain optimizers this value is ignored. Defaults to 0.'))
-        # TODO (john): Make sure I am parsing this correctly
-        parser.add_argument('--dropout_rate', required=False, type=dict,
-                            help=("A dictionary with keys 'input', 'output' and 'recurrent' "
-                                  'containing the fraction of the input, output and recurrent units'
-                                  ' to drop respectively. Values must be between 0 and 1.'))
+        parser.add_argument('--dropout_rate', required=False, nargs=3, type=float,
+                            metavar=('input', 'output', 'recurrent'),
+                            help=('Expectes three values, seperated by a space, which specify the '
+                                  'fraction of units to drop for input, output and recurrent '
+                                  'connections respectively. Values must be between 0 and 1.'))
         parser.add_argument('--fine_tune_word_embeddings', required=False, action='store_true',
                             help=('Pass this argument if pre-trained word embedding should be '
                                   'fine-tuned during training.'))
@@ -241,51 +214,37 @@ class Config(object):
         parser.add_argument('--learning_rate', required=False, type=float,
                             help=('float >= 0. Learning rate. Note that for certain optimizers '
                                   'this value is ignored'))
-        parser.add_argument('--load_pretrained_model', required=False, action='store_true', help='TODO')
+        parser.add_argument('--load_pretrained_model', required=False, action='store_true',
+                            help='TODO')
         parser.add_argument('--epochs', required=False, type=int,
                             help=('Integer. Number of epochs to train the model. An epoch is an '
                                   'iteration over all data provided.'))
         parser.add_argument('--model_name', required=False, type=str,
                             help=('Which model architecture to use. Currently, only MT-LSTM-CRF is '
                                   'provided.'))
-        # parser.add_argument('--number_of_cpu_threads', default=argument_default_value, help='')
-        # parser.add_argument('--number_of_gpus', default=argument_default_value, help='')
         parser.add_argument('--optimizer', required=False, type=str,
                             help=("Name of the optimization function to use during training. All "
                                   'optimizers implemented in Keras are supported. Defaults to '
                                   "'nadam'."))
         parser.add_argument('--output_folder', required=False, type=str,
                             help='Path to the top-level of the directory to save all output files.')
-        # parser.add_argument('--patience', default=argument_default_value, help='')
-        # parser.add_argument('--plot_format', default=argument_default_value, help='')
         parser.add_argument('--pretrained_model_weights', required=False, type=str, help='TODO')
         parser.add_argument('--replace_rare_tokens', required=False, action='store_true',
                             help=('If True, word types that occur less than 1 time in the training '
                                   'dataset will be replaced with a special unknown token.'))
-        # parser.add_argument('--reload_character_embeddings', default=argument_default_value, help='')
-        # parser.add_argument('--reload_character_lstm', default=argument_default_value, help='')
-        # parser.add_argument('--reload_crf', default=argument_default_value, help='')
-        # parser.add_argument('--reload_feedforward', default=argument_default_value, help='')
-        # parser.add_argument('--reload_token_embeddings', default=argument_default_value, help='')
-        # parser.add_argument('--reload_token_lstm', default=argument_default_value, help='')
-        # parser.add_argument('--remap_unknown_tokens_to_unk', default=argument_default_value, help='')
         parser.add_argument('--save_model', required=False, action='store_true', help='TODO')
-        # parser.add_argument('--spacylanguage', default=argument_default_value, help='')
-        # parser.add_argument('--tagging_format', default=argument_default_value, help='')
+        parser.add_argument('--tensorboard', required=False, action='store_true', help='TODO')
         parser.add_argument('--word_embed_dim', required=False, type=int,
                             help='Dimension of dense embeddings to be learned for each word.')
-        # parser.add_argument('--token_lstm_hidden_state_dimension', default=argument_default_value, help='')
         parser.add_argument('--pretrained_embeddings', required=False, type=str,
                             help='Filepath to pretrained word embeddings.')
-        # parser.add_argument('--tokenizer', default=argument_default_value, help='')
         parser.add_argument('--train_model', required=False, action='store_true', help='TODO')
-        # parser.add_argument('--use_character_lstm', default=argument_default_value, help='')
-        # parser.add_argument('--use_crf', default=argument_default_value, help='')
-        # parser.add_argument('--use_pretrained_model', default=argument_default_value, help='')
+        parser.add_argument('--variational_dropout', required=False, action='store_true',
+                            help=('Pass this flag if variational dropout should be used. NOTE THAT'
+                                  'THIS IS TEMPORARY'))
         parser.add_argument('--verbose', required=False, action='store_true', help='TODO')
 
-        # parse cli args, update this objects cli_args dictionary
+        # parse cli args, return dictionary representation of these args
         cli_args = parser.parse_args()
-        self.cli_args = vars(cli_args)
 
-        return True
+        return vars(cli_args)
