b793c9a57d1dda6e2e467d3f973c747e55a7a31c
johngiorgi <johnmgiorgi@gmail.com>
2018-08-02 14:46:57 -0400

    Changing saber structure to more of a canonical python package layout
    Former-commit-id: 60cb3f00d261af0b18f8b63ce41ef5337b61fb93
    Former-commit-id: 6a237e4dfe82983c9e79b8a96fffcec724270a0e [formerly 59aff8a892951de47157bbc2ba07e16cbcc4aa25] [formerly 20d2008eeebff7fb3dfdc824e836e36e93aa377e [formerly 13efd3fb68588fdd2e531a45152e3f1c40268a58]]
    Former-commit-id: dec51c5db967791c6f7300c0b20d1f3241a3d8b6 [formerly 65b21ab6ba400799f2ed4817bedf49440a38e561]
    Former-commit-id: 41b21c30c64ca723de3f671eda49fb5057e9b164

--- saber/dataset.py
+++ saber/dataset.py
@@ -1,113 +1,152 @@
-import os
-import glob
+"""Contains the Dataset class, which handles the loading and storage of datasets.
+"""
+import itertools
+import os.path
+from glob import glob
 
 import numpy as np
 from keras.utils import to_categorical
 
-import constants
-from preprocessor import Preprocessor
+from . import constants
+from .preprocessor import Preprocessor
 
 class Dataset(object):
-    """A class for handling data sets."""
+    """A class for handling datasets.
+
+    Args:
+        filepath (str): path to directory containing a CoNLL formatted dataset
+        sep (str): Seperator used for dataset at 'filepath'. Defaults to '\t'.
+        replace_rare_tokens (bool): True if rare tokens should be replaced
+        with a special unknown token. Threshold for considering tokens rare
+        can be found at constants.NUM_RARE.
+    """
     def __init__(self, filepath, sep='\t', replace_rare_tokens=True):
-        self.filepath = filepath
-        # search for any files in the dataset filepath ending with
-        # TRAIN_FILE_EXT or TEST_FILE_EXT
-        try:
-            self.trainset_filepath = glob.glob(os.path.join(filepath, \
-                constants.TRAIN_FILE_EXT))[0]
-            # self.testset_filepath = glob.glob(os.path.join(filepath, \
-            #   TEST_FILE_EXT))[0]
-        except IndexError:
-            print("[ERROR] No dataset found at {}".format(filepath))
+        # collect filepaths for each partition in a dictionary
+        self.partition_filepaths = self.get_filepaths(filepath)
 
         # column delimiter in CONLL dataset
         self.sep = sep
         # replace all rare tokens with special unknown token
         self.replace_rare_tokens = replace_rare_tokens
 
-        # word and tag sequences from dataset
-        self.word_seq = None
-        self.tag_seq = None
+        # word and tag sequences from dataset, a dictionary of dictionaries
+        # containing partition: type key value pairs.
+        self.type_seq = {'train': {'word': None, 'char': None, 'tag': None},
+                         'valid': {'word': None, 'char': None, 'tag': None},
+                         'test': {'word': None, 'char': None, 'tag': None},}
 
-        # word, character, and tag types from dataset
-        self.word_types = None
-        self.char_types = None
-        self.tag_types = None
+        # Word, character, and tag types from dataset
+        self.types = {'word': None, 'char': None, 'tag': None}
 
         # mappings of type: index for word, character and tag types
-        self.word_type_to_idx = None
-        self.char_type_to_idx = None
-        self.tag_type_to_idx = None
+        self.type_to_idx = {'word': None, 'char': None, 'tag': None}
         # inverse mapping from indices to tag types
-        self.idx_to_tag_type = None
+        self.idx_to_tag = None
 
-        # index sequences of words, characters and tags for training
-        self.train_word_idx_seq = None
-        self.train_char_idx_seq = None
-        self.train_tag_idx_seq = None
+        # identical to type_seq, except all types have been mapped to an integer
+        # index according to self.type_to_idx
+        self.idx_seq = {'train': {'word': None, 'char': None, 'tag': None},
+                        'valid': {'word': None, 'char': None, 'tag': None},
+                        'test': {'word': None, 'char': None, 'tag': None},}
+
+    def get_filepaths(self, filepath):
+        """Collects train set, and valid/test set filepaths, if they exists.
+
+        Looks for train, valid and test partitions at filepath/train.*, filepath/valid.* and
+        filepath/test.* respectively. Train set must be provided, valid and test sets are optional.
+
+        Args:
+            filepath (str): path to dataset (corpus)
+
+        Returns:
+            a dictionary with keys 'train', 'valid', 'test' and corresponding values containing the
+            filepaths to the train, valid and test paritions of the dataset at 'filepath'.
+
+        Raises:
+            ValueError when no file at filepath/train.* is found.
+        """
+        # acc for each partitions filepath
+        partition_filepaths = {}
+        # search for partition filepaths
+        train_partition = glob(os.path.join(filepath, constants.TRAIN_FILE))
+        valid_partition = glob(os.path.join(filepath, constants.VALID_FILE))
+        test_partition = glob(os.path.join(filepath, constants.TEST_FILE))
+
+        # must supply a train file
+        if not train_partition:
+            error_msg = "[ERROR] Must supply at least one file, train.* at {}".format(filepath)
+            raise ValueError(error_msg)
+        partition_filepaths['train'] = train_partition[0]
+
+        # optionally, valid and test files can be supplied
+        if valid_partition:
+            partition_filepaths['valid'] = valid_partition[0]
+        if test_partition:
+            partition_filepaths['test'] = test_partition[0]
+
+        return partition_filepaths
 
     def load_dataset(self, type_to_idx=None):
         """Coordinates loading of given data set at self.filepath.
 
         For a given dataset in CoNLL format at filepath, cordinates
-        the loading of data into a pandas dataframe and updates instance
-        attributes. Expects self.filepathto be a directory containing
-        a single file, train.*. If this is a compound dataset,
-        word_type_to_idx and char_type_to_idx must be provided.
+        the loading of data and updates instance attributes. Expects
+        self.filepath to be a directory containing
+        a single file, train.* and optionally two additional files, valid.* and
+        test.*. If this is a type_to_idx must be provided.
 
         Args:
-            word_type_to_idx: optional, a mapping of word types to
-                                     indices shared between datasets
-            char_type_to_idx: optional, a mapping of character types to
-                                     indices shared between datasets
+            type_to_idx (dict): optional, a mapping of word types to indices
+                (at type_to_idx['word']) and a mapping of char types to indices
+                (at type_to_idx['char']) shared between datasets.
         """
-        # if shared_by_compound is passed into function call, then this is a
+        # if type_to_idx is passed into function call, then this is a
         # compound dataset (word and char index mappings shared across datasets)
         if type_to_idx is not None:
-            self.word_type_to_idx = type_to_idx['word']
-            self.char_type_to_idx = type_to_idx['char']
+            self.type_to_idx.update(type_to_idx)
         else:
             # load data and labels from file
             self.load_data_and_labels()
             # get word, char, and tag types
             self.get_types()
             # generate type to index mappings
-            self.word_type_to_idx = Preprocessor.type_to_idx(
-                types=self.word_types,
-                initial_mapping=constants.initial_mapping_words
-            )
-            self.char_type_to_idx = Preprocessor.type_to_idx(
-                types=self.char_types,
-                initial_mapping=constants.initial_mapping_words
-            )
+            self.type_to_idx['word'] = Preprocessor.type_to_idx(
+                types=self.types['word'],
+                initial_mapping=constants.INITIAL_MAPPING_WORDS)
+            self.type_to_idx['char'] = Preprocessor.type_to_idx(
+                types=self.types['char'],
+                initial_mapping=constants.INITIAL_MAPPING_WORDS)
 
         # generate un-shared type to index mappings
-        self.tag_type_to_idx = Preprocessor.type_to_idx(
-            types=self.tag_types,
-            initial_mapping=constants.initial_mapping_tags
+        self.type_to_idx['tag'] = Preprocessor.type_to_idx(
+            types=self.types['tag'],
+            initial_mapping=constants.INITIAL_MAPPING_TAGS
         )
         # create reverse mapping of indices to tags, save computation downstream
-        self.idx_to_tag_type = {v: k for k, v in self.tag_type_to_idx.items()}
+        self.idx_to_tag = {v: k for k, v in self.type_to_idx['tag'].items()}
 
-        # get type to idx sequences
-        self.train_word_idx_seq = Preprocessor.get_type_idx_sequence(
-            seq=self.word_seq,
-            type_to_idx=self.word_type_to_idx,
-            type_='word'
-        )
-        self.train_char_idx_seq = Preprocessor.get_type_idx_sequence(
-            seq=self.word_seq,
-            type_to_idx=self.char_type_to_idx,
-            type_='char'
-        )
-        self.train_tag_idx_seq = Preprocessor.get_type_idx_sequence(
-            seq=self.tag_seq,
-            type_to_idx=self.tag_type_to_idx,
-            type_='tag'
-        )
-        # convert tag idx sequences to categorical
-        self.train_tag_idx_seq = self._tag_idx_sequence_to_categorical(self.train_tag_idx_seq)
+        # get idx sequences
+        for partition in self.partition_filepaths:
+            self.idx_seq[partition] = {
+                'word': Preprocessor.get_type_idx_sequence(
+                    seq=self.type_seq[partition]['word'],
+                    type_to_idx=self.type_to_idx['word'],
+                    type_='word'),
+                'char': Preprocessor.get_type_idx_sequence(
+                    seq=self.type_seq[partition]['word'],
+                    type_to_idx=self.type_to_idx['char'],
+                    type_='char'),
+                'tag': Preprocessor.get_type_idx_sequence(
+                    seq=self.type_seq[partition]['tag'],
+                    type_to_idx=self.type_to_idx['tag'],
+                    type_='tag')
+            }
+
+            # convert tag idx sequences to categorical
+            self.idx_seq[partition]['tag'] = \
+                self._idx_seq_to_categorical(idx_seq=self.idx_seq[partition]['tag'],
+                                             num_classes=len(self.type_to_idx['tag']))
+        return self
 
     def load_data_and_labels(self):
         """Loads CoNLL format data set given at self.trainset_filepath.
@@ -132,57 +171,63 @@ class Dataset(object):
         ...
         '''
         """
-        # global accumulators
-        word_seq, tag_seq = [], []
+        for partition, filepath in self.partition_filepaths.items():
+            # accumulate sequences across a single partition
+            word_seq, tag_seq = [], []
 
-        with open(self.trainset_filepath, 'r') as ds:
-            # local accumulators, seed with special sentence start token
-            words = []
-            tags = []
+            with open(filepath, 'r') as ds:
+                # accumulators per sentence
+                words, tags = [], []
 
-            for line in ds:
-                line = line.rstrip() # right strip
+                for line in ds:
+                    line = line.rstrip() # right strip
 
-                # accumulate each sequence
-                if len(line) == 0 or line.startswith('-DOCSTART-'):
-                    if len(words) != 0:
-                        word_seq.append(words)
-                        tag_seq.append(tags)
+                    # accumulate each sequence
+                    if not line or line.startswith('-DOCSTART-'):
+                        if words:
+                            word_seq.append(words)
+                            tag_seq.append(tags)
 
-                        words = []
-                        tags = []
+                            words, tags = [], []
 
-                # accumulate each word in a sequence
-                else:
-                    # ignore comment lines
-                    if not line.startswith('# '):
-                        word, tag = line.split('\t')
-                        words.append(word)
-                        tags.append(tag)
+                    # accumulate each word/tag in a sequence
+                    else:
+                        # ignore comment lines
+                        if not line.startswith('# '):
+                            word, tag = line.split(self.sep)
+                            words.append(word)
+                            tags.append(tag)
 
-            # in order to collect last sentence in the file
-            word_seq.append(words)
-            tag_seq.append(tags)
+                # in order to collect last sentence in the file
+                word_seq.append(words)
+                tag_seq.append(tags)
 
-        # replace all rare tokens with special unknown token
-        if self.replace_rare_tokens:
-            word_seq = Preprocessor.replace_rare_tokens(word_seq)
+            # replace all rare tokens with special unknown token
+            if self.replace_rare_tokens:
+                word_seq = Preprocessor.replace_rare_tokens(word_seq)
 
-        self.word_seq = np.asarray(word_seq)
-        self.tag_seq = np.asarray(tag_seq)
+            self.type_seq[partition] = {'word': word_seq,
+                                        'tag': tag_seq}
 
     def get_types(self):
         """Returns word types, char types and tag types.
 
         Returns the word types, character types and tag types for the current
-        loaded dataset.
+        loaded dataset. All types are shared across all partitions, that is,
+        word, char and tag types are collected from train, valid, and test
+        partitions (if provided),
 
         Returns:
             a three-tuple containing the word types, chararcter types and
             tag types.
         """
         # Word types
-        word_types = list(set([w for s in self.word_seq for w in s]))
+        # iterate over all partitions which have a none-None value at
+        # type_seq[partition]['word'], that is, all partitions present in the
+        # dataset
+        word_types = [s for p in self.type_seq.values() if p['word'] is not None for s in p['word']]
+        # flatten list of word types, and convert to set to get unqiue elements
+        word_types = list(set(list(itertools.chain.from_iterable(word_types))))
 
         # Char types
         char_types = []
@@ -191,28 +236,30 @@ class Dataset(object):
         char_types = list(set(char_types))
 
         # Tag types
-        tag_types = list(set([t for s in self.tag_seq for t in s]))
+        tag_types = [s for p in self.type_seq.values() if p['tag'] is not None for s in p['tag']]
+        tag_types = list(set(list(itertools.chain.from_iterable(tag_types))))
 
-        self.word_types = word_types
-        self.char_types = char_types
-        self.tag_types = tag_types
+        self.types['word'] = word_types
+        self.types['char'] = char_types
+        self.types['tag'] = tag_types
 
-    def _tag_idx_sequence_to_categorical(self, idx_seq):
+    def _idx_seq_to_categorical(self, idx_seq, num_classes=None):
         """One-hot encodes a given class vector.
 
-        Converts a class vector of integers, (idx_sequence) to a
-        one-hot encoded matrix. The number of classes in the one-hot encoding
-        determined by self.tag_type_count.
+        Converts a class vector of integers 'idx_seq' of shape
+        (num examples, sequence length) to a one-hot encoded matrix of shape
+        (num_examples, sequence length, num_classes).
 
         Args:
-            idx_sequence: a class vector of integers, representing a sequence
-                          of tags.
+            idx_seq: class matrix of integers of shape (num examples,
+                sequence length), representing a sequence of tags
 
         Returns:
-            one-hot endcoded matrix representation of idx_sequence.
+            numpy array, one-hot encoded matrix representation of 'idx_seq'
+            of shape (num examples, sequence length, num_classes)
         """
         # convert to one-hot encoding
-        one_hots = [to_categorical(s, len(self.tag_type_to_idx)) for s in idx_seq]
+        one_hots = [to_categorical(s, num_classes) for s in idx_seq]
         one_hots = np.array(one_hots)
 
         return one_hots
