0ae031ab65037079925df1f889147f358f40fb7f
johngiorgi <johnmgiorgi@gmail.com>
2018-08-08 14:36:37 -0400

    Adds unit tests for `Config.save()`
    Former-commit-id: 06e251ffcb80fe08e7a99035ed871735e0ceebf9
    Former-commit-id: 9eda766f5e84065dde5752926dc6ab509a12668a [formerly ecf8a79e68999f3d9fce9eb47d021cad34c2228d] [formerly a2e2a2d7cd718cdff7b2e987a0277ca8fa1ef92b [formerly a67eae98f1a85ee04a5b23a4bf0dd7a2a902b85f [formerly 9e1e2a11e6b2112a9ce04a8441801242ca58ea3e]]]
    Former-commit-id: 87863c529cf55071b72f2fdfad38105cd269f2f4 [formerly 1dbfd470a32537bc6c1b8a796845c38774ea96cf]
    Former-commit-id: 56ede0efcff76fa05bab550c9564d94a314c825b
    Former-commit-id: b24c2d3c6d590ef534b384ccf0ea7339a9197795

--- saber/tests/test_config.py
+++ saber/tests/test_config.py
@@ -1,7 +1,9 @@
 """Any and all unit tests for the Config class (saber/config.py).
 """
-import pytest
+import configparser
 from os.path import abspath
+from os.path import join
+import pytest
 
 from ..config import Config
 
@@ -26,7 +28,7 @@ DUMMY_ARGS_NO_PROCESSING = {'model_name': 'MT-LSTM-CRF',
                             'optimizer': 'nadam',
                             'activation': 'relu',
                             'learning_rate': '0.0',
-                            'grad_norm': '1',
+                            'grad_norm': '1.0',
                             'decay': '0.0',
                             'dropout_rate': '0.3, 0.3, 0.1',
                             'batch_size': '32',
@@ -57,7 +59,7 @@ DUMMY_ARGS_NO_CLI_ARGS = {'model_name': 'mt-lstm-crf',
                           'activation': 'relu',
                           'learning_rate': 0.0,
                           'decay': 0.0,
-                          'grad_norm': 1,
+                          'grad_norm': 1.0,
                           'dropout_rate': {'input': 0.3, 'output':0.3, 'recurrent': 0.1},
                           'batch_size': 32,
                           'k_folds': 2,
@@ -150,7 +152,7 @@ def test_config_attributes_no_cli_args(config_no_cli_args):
     for k, v in DUMMY_ARGS_NO_CLI_ARGS.items():
         assert v == getattr(config_no_cli_args, k)
 
-def test_config_attributes_cli_args(config_with_cli_args):
+def test_config_attributes_with_cli_args(config_with_cli_args):
     """Asserts that the class attributes of a Config object are of the expected value/type after
     object initialization, taking into account command line arguments, which take precedence over
     config arguments.
@@ -159,3 +161,66 @@ def test_config_attributes_cli_args(config_with_cli_args):
     # have overwritten our config arguments
     for k, v in DUMMY_ARGS_WITH_CLI_ARGS.items():
         assert v == getattr(config_with_cli_args, k)
+
+def test_save_no_cli_args(config_no_cli_args, tmpdir):
+    # save the config to temporary directory created by py.test
+    config_no_cli_args.save(tmpdir)
+    # load the saved config
+    saved_config = load_saved_config(tmpdir)
+
+    # need to 'unprocess' the args to check them against the saved config file
+    unprocessed_args = unprocess_args(DUMMY_ARGS_NO_CLI_ARGS)
+
+    # ensure the saved config file matches the original arguments used to create it
+    for section in CONFIG_SECTIONS:
+        for v, unprocessed_arg in zip(saved_config[section].values(), unprocessed_args):
+            assert v == unprocessed_arg
+
+def test_save_with_cli_args(config_with_cli_args, tmpdir):
+    # save the config to temporary directory created by py.test
+    config_with_cli_args.save(tmpdir)
+    # load the saved config
+    saved_config = load_saved_config(tmpdir)
+    # need to 'unprocess' the args to check them against the saved config file
+    unprocessed_args = unprocess_args(DUMMY_ARGS_WITH_CLI_ARGS)
+
+    # ensure the saved config file matches the original arguments used to create it
+    for section in CONFIG_SECTIONS:
+        for v, unprocessed_arg in zip(saved_config[section].values(), unprocessed_args):
+            assert v == unprocessed_arg
+
+# helper functions
+def load_saved_config(filepath):
+    """Load a saved ConfigParser object at 'filepath/config.ini'.
+
+    Args:
+        filepath (str): filepath to the saved config file 'config.ini'
+
+    Returns:
+        parsed ConfigParser object at 'filepath/config.ini'.
+    """
+    saved_config_filepath = join(filepath, 'config.ini')
+    saved_config = configparser.ConfigParser()
+    saved_config.read(saved_config_filepath)
+
+    return saved_config
+
+def unprocess_args(args):
+    """Unprocesses processed config args.
+
+    Given a dictionary of arguments ('arg'), returns a list containing all argument values where
+    all values have been converted to strings.
+
+    Returns:
+        a list, containing all elements of `args.values()` converted to a str representation.
+    """
+    unprocessed_args = []
+    for arg in args:
+        if isinstance(arg, list):
+            unprocessed_arg = ', '.join(arg)
+        elif isinstance(arg, dict):
+            values = [str(v) for v in arg.values()]
+            unprocessed_arg = ', '.join(values)
+        else:
+            unprocessed_arg = str(arg)
+    return unprocessed_args
