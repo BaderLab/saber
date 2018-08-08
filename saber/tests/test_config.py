ed3b86516face01e57c47eb074ea896ece477ae4
johngiorgi <johnmgiorgi@gmail.com>
2018-08-08 18:32:50 -0400

    Fixing rather large bug in unit tests for `config.py`
    Former-commit-id: cefad9f79c8d9af9489169b5d645609381361b1a
    Former-commit-id: 10aa8f23fdc2bba4e137b86e2850c1dcef9e01b5 [formerly 513063e05f08cb9ffa889b185c9c5ff6eb471b35] [formerly 75887b82f50e563da6bf8ecdd3a97f4e4b489944 [formerly bab938a7e7f8b1b9d17c6809fdbc5b97461a2bac]]
    Former-commit-id: c21d95b2d49fde5dc9d457668244e0c20ae014da [formerly 3ed9fddb75cd22225ca15af1c265327e11bd43d1]
    Former-commit-id: 4d53b6fb2277791a5acfdfdc6cdac8a498581138

--- saber/tests/test_config.py
+++ saber/tests/test_config.py
@@ -1,15 +1,13 @@
 """Any and all unit tests for the Config class (saber/config.py).
 """
 import configparser
-from os.path import abspath
-from os.path import join
+import os
 import pytest
 
 from ..config import Config
 
 # constants for dummy config/CL arguments to perform testing on
-PATH_TO_DUMMY_CONFIG = abspath('saber/tests/resources/dummy_config.ini')
-
+PATH_TO_DUMMY_CONFIG = os.path.abspath('saber/tests/resources/dummy_config.ini')
 # Sections of the .ini file
 CONFIG_SECTIONS = ['mode', 'data', 'model', 'training', 'advanced']
 
@@ -47,12 +45,13 @@ DUMMY_ARGS_NO_PROCESSING = {'model_name': 'MT-LSTM-CRF',
 DUMMY_ARGS_NO_CLI_ARGS = {'model_name': 'mt-lstm-crf',
                           'train_model': True,
                           'save_model': False,
-                          'dataset_folder': [abspath('saber/tests/resources/dummy_dataset_1')],
-                          'output_folder': abspath('../output'),
+                          'dataset_folder': \
+                                [os.path.abspath('saber/tests/resources/dummy_dataset_1')],
+                          'output_folder': os.path.abspath('../output'),
                           'pretrained_model_weights': '',
-                          'pretrained_embeddings': abspath(('saber/tests/resources/'
-                                                            'dummy_word_embeddings/'
-                                                            'dummy_word_embeddings.txt')),
+                          'pretrained_embeddings': os.path.abspath( \
+                                ('saber/tests/resources/dummy_word_embeddings/'
+                                 'dummy_word_embeddings.txt')),
                           'word_embed_dim': 200,
                           'char_embed_dim': 30,
                           'optimizer': 'nadam',
@@ -73,23 +72,23 @@ DUMMY_ARGS_NO_CLI_ARGS = {'model_name': 'mt-lstm-crf',
                           # TEMP
                           'variational_dropout': True,
                          }
-
 # Final arg values when args provided in config file and from CLI
 DUMMY_COMMAND_LINE_ARGS = {'optimizer': 'sgd',
                            'grad_norm': 1.0,
-                           'learning_rate':0.05,
-                           'decay':0.5,
+                           'learning_rate': 0.05,
+                           'decay': 0.5,
                            'dropout_rate': [0.6, 0.6, 0.2],
                           }
 DUMMY_ARGS_WITH_CLI_ARGS = {'model_name': 'mt-lstm-crf',
                             'train_model': True,
                             'save_model': False,
-                            'dataset_folder': [abspath('saber/tests/resources/dummy_dataset_1')],
-                            'output_folder': abspath('../output'),
+                            'dataset_folder': \
+                                [os.path.abspath('saber/tests/resources/dummy_dataset_1')],
+                            'output_folder': os.path.abspath('../output'),
                             'pretrained_model_weights': '',
-                            'pretrained_embeddings': abspath(('saber/tests/resources/'
-                                                              'dummy_word_embeddings/'
-                                                              'dummy_word_embeddings.txt')),
+                            'pretrained_embeddings': os.path.abspath( \
+                                ('saber/tests/resources/dummy_word_embeddings/'
+                                 'dummy_word_embeddings.txt')),
                             'word_embed_dim': 200,
                             'char_embed_dim': 30,
                             'optimizer': 'sgd',
@@ -122,35 +121,54 @@ def config_no_cli_args():
 def config_with_cli_args():
     """Returns an instance of a Config object after parsing the dummy config file with command line
     interface (CLI) args."""
-    # parse the dummy config, leave cli false and instead pass the command
-    # line args through Config.process_parameters
+    # parse the dummy config, leave cli false and instead pass command line args manually
     config = Config(filepath=PATH_TO_DUMMY_CONFIG, cli=False)
-    config._process_args(DUMMY_COMMAND_LINE_ARGS)
+    # this is a bit of a hack, but need to simulate providing commands at the command line
+    config.cli_args = DUMMY_COMMAND_LINE_ARGS
+    config.args = config._process_args(DUMMY_COMMAND_LINE_ARGS)
     return config
 
 def test_process_args_no_cli_args(config_no_cli_args):
-    """Asserts the Config.config object contains the expected values after initializing a Config
+    """Asserts the Config.config object contains the expected attributes after initializing a Config
     object without CLI args."""
+    # check filepath attribute
+    assert config_no_cli_args.filepath == os.path.join(os.path.dirname( \
+        os.path.os.path.abspath(__file__)), PATH_TO_DUMMY_CONFIG)
+    # check that the config file contains the same values as DUMMY_ARGS_NO_PROCESSING
     config = config_no_cli_args.config
     for section in CONFIG_SECTIONS:
-        for k, v in config[section].items():
-            assert v == DUMMY_ARGS_NO_PROCESSING[k]
+        for arg, value in config[section].items():
+            assert value == DUMMY_ARGS_NO_PROCESSING[arg]
+    # check cli_args attribute
+    assert config_no_cli_args.cli_args == {}
+    # check args attribute
+    for arg, value in config_no_cli_args.args.items():
+        assert value == DUMMY_ARGS_NO_CLI_ARGS[arg]
 
 def test_process_args_with_cli_args(config_with_cli_args):
-    """Asserts the Config.config object contains the expected values after initializing a Config
+    """Asserts the Config.config object contains the expected attributes after initializing a Config
     object with CLI args."""
+    # check filepath attribute
+    assert config_with_cli_args.filepath == os.path.join(os.path.dirname( \
+        os.path.os.path.abspath(__file__)), PATH_TO_DUMMY_CONFIG)
     config = config_with_cli_args.config
+    # check that the config file contains the same values as DUMMY_ARGS_NO_PROCESSING
     for section in CONFIG_SECTIONS:
-        for k, v in config[section].items():
-            assert v == DUMMY_ARGS_NO_PROCESSING[k]
+        for arg, value in config[section].items():
+            assert value == DUMMY_ARGS_NO_PROCESSING[arg]
+    # check cli_args attribute
+    assert config_with_cli_args.cli_args == DUMMY_COMMAND_LINE_ARGS
+    # check args attribute
+    for arg, value in config_with_cli_args.args.items():
+        assert value == DUMMY_ARGS_WITH_CLI_ARGS[arg]
 
 def test_config_attributes_no_cli_args(config_no_cli_args):
     """Asserts that the class attributes of a Config object are of the expected value/type after
     objects initialization, with NO command line arguments.
     """
     # check that we get the values we expected
-    for k, v in DUMMY_ARGS_NO_CLI_ARGS.items():
-        assert v == getattr(config_no_cli_args, k)
+    for arg, value in DUMMY_ARGS_NO_CLI_ARGS.items():
+        assert value == getattr(config_no_cli_args, arg)
 
 def test_config_attributes_with_cli_args(config_with_cli_args):
     """Asserts that the class attributes of a Config object are of the expected value/type after
@@ -159,10 +177,11 @@ def test_config_attributes_with_cli_args(config_with_cli_args):
     """
     # check that we get the values we expected, specifically, check that our command line arguments
     # have overwritten our config arguments
-    for k, v in DUMMY_ARGS_WITH_CLI_ARGS.items():
-        assert v == getattr(config_with_cli_args, k)
+    for arg, value in DUMMY_ARGS_WITH_CLI_ARGS.items():
+        assert value == getattr(config_with_cli_args, arg)
 
 def test_save_no_cli_args(config_no_cli_args, tmpdir):
+    """Asserts that a saved config file contains the correct arguments and values."""
     # save the config to temporary directory created by py.test
     config_no_cli_args.save(tmpdir)
     # load the saved config
@@ -173,21 +192,25 @@ def test_save_no_cli_args(config_no_cli_args, tmpdir):
 
     # ensure the saved config file matches the original arguments used to create it
     for section in CONFIG_SECTIONS:
-        for v, unprocessed_arg in zip(saved_config[section].values(), unprocessed_args):
-            assert v == unprocessed_arg
+        for arg, value in saved_config[section].items():
+            assert value == unprocessed_args[arg]
 
 def test_save_with_cli_args(config_with_cli_args, tmpdir):
+    """Asserts that a saved config file contains the correct arguments and values, taking into
+    account command line arguments, which take precedence over config arguments.
+    """
     # save the config to temporary directory created by py.test
     config_with_cli_args.save(tmpdir)
     # load the saved config
     saved_config = load_saved_config(tmpdir)
+
     # need to 'unprocess' the args to check them against the saved config file
     unprocessed_args = unprocess_args(DUMMY_ARGS_WITH_CLI_ARGS)
 
     # ensure the saved config file matches the original arguments used to create it
     for section in CONFIG_SECTIONS:
-        for v, unprocessed_arg in zip(saved_config[section].values(), unprocessed_args):
-            assert v == unprocessed_arg
+        for arg, value in saved_config[section].items():
+            assert value == unprocessed_args[arg]
 
 # helper functions
 def load_saved_config(filepath):
@@ -199,7 +222,7 @@ def load_saved_config(filepath):
     Returns:
         parsed ConfigParser object at 'filepath/config.ini'.
     """
-    saved_config_filepath = join(filepath, 'config.ini')
+    saved_config_filepath = os.path.join(filepath, 'config.ini')
     saved_config = configparser.ConfigParser()
     saved_config.read(saved_config_filepath)
 
@@ -208,19 +231,22 @@ def load_saved_config(filepath):
 def unprocess_args(args):
     """Unprocesses processed config args.
 
-    Given a dictionary of arguments ('arg'), returns a list containing all argument values where
-    all values have been converted to strings.
+    Given a dictionary of arguments ('arg'), returns a dictionary where all values have been
+    converted to string representation.
 
     Returns:
-        a list, containing all elements of `args.values()` converted to a str representation.
+        args, where all values have been replaced by a str representation.
     """
-    unprocessed_args = []
-    for arg in args:
-        if isinstance(arg, list):
-            unprocessed_arg = ', '.join(arg)
-        elif isinstance(arg, dict):
-            values = [str(v) for v in arg.values()]
-            unprocessed_arg = ', '.join(values)
+    unprocessed_args = {}
+    for arg, value in args.items():
+        if isinstance(value, list):
+            unprocessed_arg = ', '.join(value)
+        elif isinstance(value, dict):
+            dict_values = [str(v) for v in value.values()]
+            unprocessed_arg = ', '.join(dict_values)
         else:
-            unprocessed_arg = str(arg)
+            unprocessed_arg = str(value)
+
+        unprocessed_args[arg] = unprocessed_arg
+
     return unprocessed_args
