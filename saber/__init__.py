cd00ca2a5e4f0a9494f633e15f07c58ed7ac0703
johngiorgi <johnmgiorgi@gmail.com>
2018-08-13 12:05:16 -0400

    Setup package wide logging in `__init__.py` :wrench:
    Former-commit-id: 0703f4379095d799ec099a8433ec93415d424ab5
    Former-commit-id: eb5db30f5c295b2c0e42f1f4133be80db7d27a42 [formerly 665ff5520cdc2f206ca75798198753ea46a678b1] [formerly e4834b72feb2bfd3d2efb2cb150ee62de76bc2ca [formerly 266d8232776a02078f07ac68eff04a060667f8ed]]
    Former-commit-id: 912bbd219dbfd3c43122a2e9a30125a7aee021de [formerly fec317209b47d00818bf9ccf1e8f2bad28ea2180]
    Former-commit-id: 480d48372cf3403f393c859f302370f92c2eaa4b

---
+++ saber/__init__.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+import os
+import logging
+
+from . import constants
+
+print('Saber version: {0}'.format(constants.__version__))
+
+# Set Tensforflow logging level
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+# If applicable, delete the existing log file to generate a fresh log file during each execution
+if os.path.isfile("saber.log"):
+    os.remove("saber.log")
+
+# Create the Logger
+logging.basicConfig(filename="saber.log",
+                    level=logging.DEBUG,
+                    format='%(name)s - %(levelname)s - %(message)s')
+
+# Log the date to start
+logging.info('Saber invocation: %s\n%s', datetime.now(), '=' * 75)
