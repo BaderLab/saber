aa8f167fa45bf0e557aed1039e4025f7f60ffcd0
johngiorgi <johnmgiorgi@gmail.com>
2018-08-14 17:30:02 -0400

    Drop `bin_to_text`, no need for it anymore
    Former-commit-id: 291b921cf198ae6b487d237ca6a9aeb440b31d09
    Former-commit-id: eb66e2b5647a2fc8e091a4d8a20e31ad0556d6c2 [formerly d5080b41b000d95a1d801579225bde103ebcd7f6] [formerly 8b1f34ae38fc394d8d243e2b710c8c55861c11dd [formerly 5e6e34a0a140437f66ec0ff43eaa30c797dbebad [formerly c551f5a0c34524babbb230744a60fc0bfb150896]]]
    Former-commit-id: a17ab97110beb66dbee03e9e52d57b085506272a [formerly e39fc841495a43318c2a5dab6f7355e395b8827f]
    Former-commit-id: 3e9417a755770dfcccff5e5cb63aca88c7a09ea8
    Former-commit-id: c35ed70329edaad08ca92d854f9cdbc259c293dc

--- saber/utils/generic_utils.py
+++ saber/utils/generic_utils.py
@@ -74,35 +74,6 @@ def compress_model(dir_path):
 
     return True
 
-def bin_to_txt(filepath, output_dir=os.getcwd()):
-    """Converts word embeddings given in the binary C format (w2v) to a simple
-    text format that can be used with Saber.
-
-    Args:
-        filepath (str): path to the word vectors file in binary C (w2v) format
-        output_dir (str): path to save converted word vectors file (defaults to
-            current working directory)
-    """
-    # bad practice, but requires gensim to be installed when using
-    # SequenceProcessor if import statement is at the top of this file
-    from gensim.models.keyedvectors import KeyedVectors
-
-    # load word vectors provided in C binary format
-    word_vectors = KeyedVectors.load_word2vec_format(filepath, binary=True)
-    vocab = word_vectors.vocab
-
-    # create a new filepath
-    base_name = os.path.splitext(os.path.basename(filepath))[0]
-    output_filepath = os.path.join(output_dir, base_name + '.txt')
-
-    # write contents of input_file to new file output_filepath in txt format
-    with codecs.open(output_filepath, 'w+', encoding='utf-8') as out_file:
-        for word in vocab:
-            vector = word_vectors[word]
-            out_file.write("%s %s\n" %(word, " ".join(str(v) for v in vector)))
-
-    print('[INFO] Converted C binary file saved to {}'.format(output_filepath))
-
 def get_pretrained_model_dir(config):
     """Returns path to top-level directory to save a pretrained model.
 
