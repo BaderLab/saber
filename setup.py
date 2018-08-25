0c3856209cdd247e76887a9e770959a7d12d7d4d
johngiorgi <johnmgiorgi@gmail.com>
2018-08-24 20:46:28 -0400

    :bug: fixes issue with conflicting dependencies
    Former-commit-id: 6bf6e563fce1a9723ac949f2607b60c2439a9333
    Former-commit-id: 76b95c9cb27aa8f0eb726a18271fbe21e3ccfb2b [formerly ba06f1280b663b0cbb716562fb3862ff27ab6e41] [formerly 455499e569b1c064d8a7e60a0b8a7fe8e92f67ef [formerly f1038794f21264f0319867f26b57d011f196a4d6 [formerly 2ce77e5225893a4f41b8a3e82c40e4eb7b3b6245]]]
    Former-commit-id: a5120d406c74924db008ff20c347e965c98a7c0d [formerly 7b7b8573d7c76f5520ad0b05f6e2678cbf3bbbb8]
    Former-commit-id: fab2f267023a222a0c433e6a750430cb506e2c84
    Former-commit-id: b16e2143e5136bb239f862bbff833e6bbef341b0

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setuptools.setup(
     version="0.0.1",
     author="John Giorgi",
     author_email="johnmgiorgi@gmail.com",
+    license="MIT",
     description="Saber: Sequence Annotator for Biomedical Entities and Relations",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -22,12 +23,13 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ),
     install_requires=[
-        'keras>=2.2.0',
         'tensorflow>=1.9.0',
+        'Flask>=1.0.2',
         'gensim>=3.4.0'
+        'keras==2.2.0',
+        'PTable>=0.9.2',
         'scikit-learn>=0.19.1',
         'spacy>=2.0.11',
-        'Flask>=1.0.2',
-        'PTable>=0.9.2',
     ],
+    include_package_data=True,
 )
