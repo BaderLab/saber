a079d8c66c92a3f2270e21b02c4c244345a6d2b1
johngiorgi <johnmgiorgi@gmail.com>
2018-08-26 09:46:33 -0400

    :recycle: All references to internal files now relative

--- saber/tests/test_multi_task_lstm_crf.py
+++ saber/tests/test_multi_task_lstm_crf.py
@@ -1,23 +1,22 @@
-from os.path import abspath
-import pytest
-
 import numpy
+import os
+import pytest
 
 from ..config import Config
 from ..sequence_processor import SequenceProcessor
-
-PATH_TO_DUMMY_CONFIG = abspath('saber/tests/resources/dummy_config.ini')
-PATH_TO_DUMMY_DATASET = abspath('saber/tests/resources/dummy_dataset_1')
-PATH_TO_DUMMY_TOKEN_EMBEDDINGS = abspath('saber/tests/resources/dummy_word_embeddings/dummy_word_embeddings.txt')
+from ..constants import PATH_TO_DUMMY_CONFIG
+from ..constants import PATH_TO_DUMMY_DATASET
+from ..constants import PATH_TO_DUMMY_EMBEDDINGS
 
 # TODO (johngiorgi): fix some of the test_model_attributes_after_creation_of_model tests
 
 @pytest.fixture
 def dummy_config():
-    """Returns an instance of a configparser object after parsing the dummy
-    config file. """
-    # create a dictionary to serve as cli arguments
-    cli_arguments = {'dataset_folder': [PATH_TO_DUMMY_DATASET]}
+    """Returns an instance of a configparser object after parsing the dummy config file."""
+    # the dataset and embeddings are used for test purposes so they must point to the
+    # correct resources, this can be ensured by passing their paths here
+    cli_arguments = {'dataset_folder': [PATH_TO_DUMMY_DATASET],
+                     'pretrained_embeddings': PATH_TO_DUMMY_EMBEDDINGS}
     # create the config object, taking into account the CLI args
     dummy_config = Config(PATH_TO_DUMMY_CONFIG)
     dummy_config._process_args(cli_arguments)
@@ -26,9 +25,10 @@ def dummy_config():
 
 @pytest.fixture
 def multi_task_lstm_crf_single_model(dummy_config):
-    """Returns an instance of MultiTaskLSTMCRF initialized with the
-    default configuration file and a single compiled model."""
+    """Returns an instance of MultiTaskLSTMCRF initialized with the default configuration file and
+    a single compiled model."""
     seq_processor_with_single_ds = SequenceProcessor(config=dummy_config)
+
     seq_processor_with_single_ds.load_dataset()
     seq_processor_with_single_ds.load_embeddings()
     seq_processor_with_single_ds.create_model()
@@ -48,6 +48,7 @@ def test_model_attributes_after_creation_of_model(multi_task_lstm_crf_single_mod
     assert multi_task_lstm_crf_single_model.config.batch_size == 32
     assert multi_task_lstm_crf_single_model.config.char_embed_dim == 30
     assert multi_task_lstm_crf_single_model.config.criteria == 'exact'
+    # TEMP: need a better solution than this
     assert multi_task_lstm_crf_single_model.config.dataset_folder == [PATH_TO_DUMMY_DATASET]
     assert not multi_task_lstm_crf_single_model.config.debug
     assert multi_task_lstm_crf_single_model.config.decay == 0.0
@@ -59,10 +60,11 @@ def test_model_attributes_after_creation_of_model(multi_task_lstm_crf_single_mod
     assert multi_task_lstm_crf_single_model.config.epochs == 50
     assert multi_task_lstm_crf_single_model.config.model_name == 'mt-lstm-crf'
     assert multi_task_lstm_crf_single_model.config.optimizer == 'nadam'
-    assert multi_task_lstm_crf_single_model.config.output_folder == abspath('../output')
+    assert multi_task_lstm_crf_single_model.config.output_folder == os.path.abspath('../output')
     assert multi_task_lstm_crf_single_model.config.pretrained_model_weights == ''
     assert not multi_task_lstm_crf_single_model.config.replace_rare_tokens
     assert multi_task_lstm_crf_single_model.config.word_embed_dim == 200
-    assert multi_task_lstm_crf_single_model.config.pretrained_embeddings == PATH_TO_DUMMY_TOKEN_EMBEDDINGS
+    # TEMP: need a better solution than this
+    assert multi_task_lstm_crf_single_model.config.pretrained_embeddings == PATH_TO_DUMMY_EMBEDDINGS
     assert multi_task_lstm_crf_single_model.config.train_model
     assert not multi_task_lstm_crf_single_model.config.verbose
