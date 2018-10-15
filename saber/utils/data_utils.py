632a60eb9ffdf25f613f8074e72634f72edcb213
johngiorgi <johnmgiorgi@gmail.com>
2018-10-15 12:41:38 -0400

    :recycle: Refactor utils

---
+++ saber/utils/data_utils.py
@@ -0,0 +1,289 @@
+"""A collection of data-related helper/utility functions.
+"""
+import glob
+import logging
+import os
+from itertools import chain
+
+import numpy as np
+from keras.utils import to_categorical
+from sklearn.model_selection import KFold, train_test_split
+
+from .. import constants
+from ..preprocessor import Preprocessor
+
+LOGGER = logging.getLogger(__name__)
+
+def get_filepaths(filepath):
+    """Returns dictionary with filepaths to `train`/`valid`/`test` partitions from `filepath`.
+
+    Returns a dictionary containing filepaths to `train`, `valid` and `test` partitions at
+    `filepath/train.*``, `filepath/valid.*` and `filepath/test.*`. A train set must be provided,
+    while valid and test sets are optional.
+
+    Args:
+        filepath (str): Path to dataset folder.
+
+    Returns:
+        A dictionary with keys `train`, and optionally, `valid` and `test` containing the
+        filepaths to the train, valid and test paritions of the dataset at `filepath`.
+
+    Raises:
+        ValueError when no file at `filepath/train.*` is found.
+    """
+    partition_filepaths = {}
+    # search for partition filepaths
+    train_partition = glob.glob(os.path.join(filepath, constants.TRAIN_FILE))
+    valid_partition = glob.glob(os.path.join(filepath, constants.VALID_FILE))
+    test_partition = glob.glob(os.path.join(filepath, constants.TEST_FILE))
+
+    # must supply a train file
+    if not train_partition:
+        err_msg = "Must supply at least one file, train.* at {}".format(filepath)
+        LOGGER.error('ValueError %s', err_msg)
+        raise ValueError(err_msg)
+
+    # collect filepaths in a dictionary, valid and test files are optional
+    partition_filepaths['train'] = train_partition[0]
+    partition_filepaths['valid'] = valid_partition[0] if valid_partition else None
+    partition_filepaths['test'] = test_partition[0] if test_partition else None
+
+    return partition_filepaths
+
+def load_single_dataset(config):
+    """Loads a single dataset.
+
+    Creates and loads a single dataset object for a dataset at `config.dataset_folder[0]`.
+
+    Args:
+        config (Config): A Config object which contains a set of harmonzied arguments provided in
+            a *.ini file and, optionally, from the command line.
+
+    Returns:
+        A list containing a single Dataset object.
+    """
+    from ..dataset import Dataset # breaks circular import
+
+    dataset = Dataset(directory=config.dataset_folder[0],
+                      replace_rare_tokens=config.replace_rare_tokens)
+    dataset.load()
+
+    return [dataset]
+
+def load_compound_dataset(config):
+    """Loads a compound dataset.
+
+    Creates and loads a `compound` dataset. Compound datasets are specified by multiple
+    individual datasets, and share multiple attributes (such as `word` and `char` type to index
+    mappings). Loads such a dataset for each dataset at `config.dataset_folder`.
+
+    Args:
+        config (Config): A Config object which contains a set of harmonzied arguments provided in
+            a *.ini file and, optionally, from the command line.
+
+    Returns:
+        A list containing multiple Dataset objects.
+    """
+
+    from ..dataset import Dataset # breaks circular import
+    # accumulate and load each dataset
+    compound_ds = [Dataset(directory=dir_, replace_rare_tokens=config.replace_rare_tokens)
+                   for dir_ in config.dataset_folder]
+    for dataset in compound_ds:
+        dataset.load()
+
+    # to generate a compound dataset, we need to:
+    # 1. pool word and char types
+    # 2. compute mappings of these pooled types to unique integer IDs
+    # 3. update each datasets type_to_idx mappings
+    # 4. re-compute the index sequences
+
+    # 1. pool word and char types
+    combined_types = {'word': [ds.types['word'] for ds in compound_ds],
+                      'char': [ds.types['char'] for ds in compound_ds]}
+    combined_types['word'] = list(set(chain.from_iterable(combined_types['word'])))
+    combined_types['char'] = list(set(chain.from_iterable(combined_types['char'])))
+
+    # 2. compute mappings of these pooled types to unique integer IDs
+    type_to_idx = {
+        'word': Preprocessor.type_to_idx(types=combined_types['word'],
+                                         initial_mapping=constants.INITIAL_MAPPING['word']),
+        'char': Preprocessor.type_to_idx(types=combined_types['char'],
+                                         initial_mapping=constants.INITIAL_MAPPING['word']),
+    }
+
+    for dataset in compound_ds:
+        # 3. update each datasets type_to_idx mappings
+        dataset.type_to_idx.update(type_to_idx)
+        # 4. re-compute the index sequences
+        dataset.get_idx_seq()
+
+    return compound_ds
+
+def setup_dataset_for_transfer(dataset, type_to_idx):
+    """Modifys a `Dataset` object when transfer learning.
+
+    Performs a series of steps to setup a loaded Dataset object (`dataset`) to serve as the
+    target dataset when transfer learning.
+
+    Args:
+        dataset (Dataset): A Dataset object for which `Dataset.load()` has been called.
+        type_to_idx (dict): A dictionary mapping word, char and tag types to unique integer IDs.
+    """
+    dataset.type_to_idx.update(type_to_idx)
+    # ensures that tags present in both source and target datasets map to same ID
+    dataset.type_to_idx['tag'] = Preprocessor.type_to_idx(dataset.types['tag'],
+                                                          type_to_idx['tag'])
+    dataset.get_idx_to_tag() # re-generate index to tag mapping
+    dataset.get_idx_seq() # re-generate index sequence
+
+def one_hot_encode(idx_seq):
+    """One-hot encodes a given class vector.
+
+    Converts a class matrix of integers, `idx_seq`, of shape (num examples, sequence length) to a
+    one-hot encoded matrix of shape (num_examples, sequence length, num_classes).
+
+    Args:
+        idx_seq: class matrix of integers of shape (num examples, sequence length), representing a
+            sequence of tags.
+
+    Returns:
+        numpy array, one-hot encoded matrix representation of `idx_seq` of shape
+            (num examples, sequence length, num_classes)
+    """
+    # convert to one-hot encoding
+    one_hots = [to_categorical(s) for s in idx_seq]
+    one_hots = np.array(one_hots)
+
+    return one_hots
+
+def collect_valid_data(training_data, test_size=0.10):
+    """Splits training data (`training_data`) into train and validation partitions.
+
+    Generates a new training set of size 1 - `test_size` and a validation set of size `test_size`
+    from the original training data given in `training_data`. Expects `training_data` to be a
+    dictionary with the keys 'x_train' and 'y_train', containing the input examples and targets
+    respectively.
+
+    Args:
+        training_data (dict): A dictionary containing training data (inputs and targets).
+        test_size (float): Percentage of the training examples in `training_data` to use for the new
+            validation set.
+
+    Returns:
+        `training_data`, containing new train and validation sets at keys 'x_train'/'y_train' and
+            'x_valid'/'y_valid' respectively.
+
+    Raises:
+        ValueError if `training_data` does not contain the keys 'x_train', 'y_train'.
+    """
+    if any(['x_train' not in data or 'y_train' not in data for data in training_data]):
+        err_msg = "Argument `training_data` must contain the keys 'x_train' and 'y_train'"
+        LOGGER.error("ValueError: %s", err_msg)
+        raise ValueError(err_msg)
+
+    for i, data in enumerate(training_data):
+        x_train_word, x_valid_word, x_train_char, x_valid_char, y_train, y_valid = \
+            train_test_split(data['x_train'][0],
+                             data['x_train'][1],
+                             data['y_train'],
+                             test_size=test_size,
+                             random_state=42,
+                             shuffle=False)
+
+        training_data[i] = {'x_train': [x_train_word, x_train_char],
+                            'x_valid': [x_valid_word, x_valid_char],
+                            'y_train': y_train,
+                            'y_valid': y_valid,
+                           }
+
+    return training_data
+
+def get_train_valid_indices(training_data, k_folds):
+    """Get `k_folds` number of sets of train/valid indices for all datasets in `datasets`.
+
+    For all Dataset objects in `datasets`, gets `k_folds` number of train and valid indices. Returns
+    a list of lists of two-tuples, where the outer list is of length `len(datasets)` and the inner
+    list is of length `k_folds` and contains two-tuples corresponding to train and valid indices
+    respectively. For example, the train indices for the ith dataset and jth fold would be
+    train_valid_indices[i][j][0].
+
+    Args:
+        datasets (list): a list of Dataset objects.
+        k_folds (int): Number of folds to compute indices for.
+
+    Returns:
+        A list of lists of two-tuples, where index [i][j] is a tuple containing the train and valid
+        indicies (in that order) for the ith dataset and jth k-fold.
+    """
+    train_valid_indices = [] # acc
+    kf = KFold(n_splits=k_folds, random_state=42) # Sklearn KFold object
+
+    for i, _ in enumerate(training_data):
+        X, _ = training_data[i]['x_train']
+        train_valid_indices.append([(ti, vi) for ti, vi in kf.split(X)])
+
+    return train_valid_indices
+
+def get_data_partitions(training_data, train_valid_indices):
+    """Get train and valid partitions for all k-folds for all datasets.
+
+    For all Dataset objects in `datasets`, gets the train and valid partitions for the current
+    k-fold (`fold`) using the indices given at `train_valid_indices`. Returns a list of four-tuples:
+
+    (x_train, x_valid, y_train, y_valid)
+
+    Where x represents the inputs, and y the labels or targets.
+
+    Args:
+        datasets (list): a list of Dataset objects.
+        train_valid_indices (list): A list of list of two-tuples, where train_valid_indices[i][j].
+            is a tuple containing the train and valid indices (in that order) for the ith dataset.
+            and jth fold
+        fold (int): The current fold in k-fold cross-validation.
+
+    Returns:
+        A list of four-tuples containing train and valid data for all datasets.
+    """
+    partitioned_data = []
+    for i, _ in enumerate(train_valid_indices): # loop over datasets
+        partitioned_data.append([])
+        for j, _ in enumerate(train_valid_indices[i]): # loop over folds
+
+            # train_valid_indices[i][j] is a two-tuple which contains the train and valid indices
+            train_indices, valid_indices = train_valid_indices[i][j]
+            # get inputs and targets for dataset i
+            x_word_train, x_char_train = training_data[i]['x_train']
+            y_train = training_data[i]['y_train']
+
+            # create and accumulate train/valid partitions
+            x_word_train, x_word_valid = x_word_train[train_indices], x_word_train[valid_indices]
+            x_char_train, x_char_valid = x_char_train[train_indices], x_char_train[valid_indices]
+            y_train, y_valid = y_train[train_indices], y_train[valid_indices]
+
+            partitioned_data[i].append({'x_train': [x_word_train, x_char_train],
+                                        'x_valid': [x_word_valid, x_char_valid],
+                                        'y_train': y_train,
+                                        'y_valid': y_valid,})
+
+    return partitioned_data
+
+def collect_cv_data(training_data, k_folds):
+    """Splits training data (`training_data`) into `k_folds` number of train/valid partitions.
+
+    Generates a train/valid split for `k_folds` number of cross-validation folds from the data
+    given in `training_data`. Returns a list of lists, `partitioned_data`, where
+    `partitioned_data[i][j]` contains the data for the ith dataset and jth fold.
+
+    Args:
+        training_data (dict): A dictionary containing training data (inputs and targets).
+        k_folds (int): Number of folds to split the data into.
+
+    Returns:
+        A list of lists, `partitioned_data`, where `partitioned_data[i][j]` contains the data for
+        the ith dataset and jth fold.
+    """
+    train_valid_indices = get_train_valid_indices(training_data, k_folds)
+    partitioned_data = get_data_partitions(training_data, train_valid_indices)
+
+    return partitioned_data
--- saber/utils/generic_utils.py
+++ saber/utils/generic_utils.py
@@ -12,77 +12,55 @@ from .. import constants
 log = logging.getLogger(__name__)
 
 # https://stackoverflow.com/questions/273192/how-can-i-create-a-directory-if-it-does-not-exist#273227
-def make_dir(directory_filepath):
-    """Creates a directory (directory_filepath) if it does not exist.
-
-    Args:
-        directory_filepath: filepath of directory to create
+def make_dir(directory):
+    """Creates a directory at `directory` if it does not already exist.
     """
-    # create output directory if it does not exist
     try:
-        os.makedirs(directory_filepath)
+        os.makedirs(directory)
     except OSError as err:
         if err.errno != errno.EEXIST:
             raise
 
 def clean_path(filepath):
     """Returns normalized and absolutized `filepath`.
-
-    Args:
-        filepath (str): filepath to be normalized and absolutized
-
-    Returns:
-        `filepath`, normalized and absolutized
     """
     filepath = filepath.strip() if isinstance(filepath, str) else filepath
     return os.path.abspath(os.path.normpath(filepath))
 
-def decompress_model(filepath):
-    """Decompresses a bz2 compressed Saber model.
-
-    If filepath is not a directory, decompresses the identically named bz2 Saber
-    model at filepath.
-
-    Args:
-        filepath (str): path to a pre-trained Saber model (zipped or unzipped)
+def extract_directory(directory):
+    """Extracts bz2 compressed directory at `directory` if directory is compressed.
     """
-    if not os.path.isdir(filepath):
-        head, _ = os.path.split(os.path.abspath(filepath))
+    if not os.path.isdir(directory):
+        head, _ = os.path.split(os.path.abspath(directory))
 
-        print('Unzipping pretrained model... '.format(), end='', flush=True)
-        unpack_archive(filepath + '.tar.bz2', extract_dir=head)
-        print('Done.')
+        print('\nunzipping... ', end='', flush=True)
+        unpack_archive(directory + '.tar.bz2', extract_dir=head)
 
-def compress_model(dir_path):
+def compress_directory(directory):
     """Compresses a given directory using bz2 compression.
 
-    Args:
-        dir_path (str): path to directory to compress.
-
-    Returns:
-        True if compression completed without error.
-
     Raises:
-        ValueError: if no directory at 'dir_path' exists or if 'dir_path'.tar.bz2 already exists.
+        ValueError: if no directory at `directory` exists or if `directory`.tar.bz2 already exists.
     """
-    # clean/normalize dir_path
-    dir_path = os.path.abspath(os.path.normcase(os.path.normpath(dir_path)))
+    # clean/normalize directory
+    directory = os.path.abspath(os.path.normcase(os.path.normpath(directory)))
 
-    output_filepath = '{}.tar.bz2'.format(dir_path)
+    # raise ValueError if directory.tar.bz2 already exists or if directory not valid
+    output_filepath = '{}.tar.bz2'.format(directory)
     if os.path.exists(output_filepath):
         err_msg = "{} already exists.".format(output_filepath)
         log.error('ValueError %s', err_msg)
         raise ValueError(err_msg)
-    if not os.path.exists(dir_path):
-        err_msg = "File or directory at 'dir_path' does not exist."
+    if not os.path.exists(directory):
+        err_msg = "File or directory at 'directory' does not exist."
         log.error('ValueError %s', err_msg)
         raise ValueError(err_msg)
 
     # create bz2 compressed directory, remove uncompressed directory
-    root_dir = os.path.abspath(''.join(os.path.split(dir_path)[:-1]))
-    base_dir = os.path.basename(dir_path)
-    shutil.make_archive(base_name=dir_path, format='bztar', root_dir=root_dir, base_dir=base_dir)
-    shutil.rmtree(dir_path)
+    root_dir = os.path.abspath(''.join(os.path.split(directory)[:-1]))
+    base_dir = os.path.basename(directory)
+    shutil.make_archive(base_name=directory, format='bztar', root_dir=root_dir, base_dir=base_dir)
+    shutil.rmtree(directory)
 
 def get_pretrained_model_dir(config):
     """Returns path to top-level directory to save a pretrained model.
@@ -90,14 +68,14 @@ def get_pretrained_model_dir(config):
     Returns a directory path to save a pretrained model based on `config.dataset_folder` and
     `config.output_folder`. The folder which contains the saved model is named from each dataset
     name in `config.dataset_folder` joined by an underscore. The full path is:
-    "config.output_folder/constants.PRETRAINED_MODEL_DIR/dataset_names"
+    `<config.output_folder>/<constants.PRETRAINED_MODEL_DIR>/<dataset_names>`
 
     Args:
         config (Config): Config object
 
     Returns:
-        full path to save a pre-trained model based on `config.dataset_folder` and
-        `config.dataset_folder`
+        full path to save a pre-trained model based on 'config.dataset_folder' and
+        'config.dataset_folder'
     """
     ds_names = '_'.join([os.path.basename(ds) for ds in config.dataset_folder])
     return os.path.join(config.output_folder, constants.PRETRAINED_MODEL_DIR, ds_names)
