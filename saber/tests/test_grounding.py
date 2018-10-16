faa6acd5f56bee7f1b65b5884408cf22fb3fc217
IgorRodchenkov <rodche@gmail.com>
2018-10-16 15:42:33 -0400

    Added entity grounding module and test stubs.
    Former-commit-id: fabedeafb12d6a31f7882f98824b8ae487e840ad
    Former-commit-id: d67829e213e8ec2b3a859658fd4c52358465422f [formerly 5cf98f8fe98d6df5d1296e4a2545eebe277e7794] [formerly b6594206326b235dcf2396ea3b2163d3f79361db [formerly 4c5b2c94e8c320948834169d17cd3a156f8eaa7e]]
    Former-commit-id: e78bc61d3b576a7fd993a50806fb211c1c75a3ff [formerly f341663fe3b86f9f71da0439ada65c369b21e28c]
    Former-commit-id: c0d2dc24ea94723debc670accccefc9cdc433e18

--- saber/tests/test_grounding.py
+++ saber/tests/test_grounding.py
@@ -1,2 +1,38 @@
 import pytest
 
+from ..utils import grounding
+
+@pytest.fixture
+def annotate():
+    return {
+    "ents": [
+        {
+            "end": 27,
+            "label": "PRGE",
+            "start": 23,
+            "text": "Hdm2"
+        },
+        {
+            "end": 34,
+            "label": "PRGE",
+            "start": 31,
+            "text": "MK2"
+        },
+        {
+            "end": 69,
+            "label": "PRGE",
+            "start": 66,
+            "text": "p53"
+        }
+    ],
+    "text": "The phosphorylation of Hdm2 by MK2 promotes the ubiquitination of p53.",
+    "title": ""
+  }
+
+def test_uniprot_search():
+    annotation = annotate()
+    print(str(annotation))
+
+    assert len(grounding.uniprot_search("MK2"))==0
+
+#
