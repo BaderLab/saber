e9dc4c18c260e292bbeb85cf68d9f69404719427
John Giorgi <johnmgiorgi@gmail.com>
2018-10-18 09:39:55 -0400

    :recycle: cleanup / lint
    Former-commit-id: 1463c9ed8547e6a29829c996160a21e7eb3ff740

--- saber/utils/grounding_utils.py
+++ saber/utils/grounding_utils.py
@@ -1,7 +1,10 @@
+"""A collection of helper/utility functions for grounding entities.
+"""
 import logging
 
 import requests
 
+LOGGER = logging.getLogger(__name__)
 
 def _query_uniprot(text, organisms=('9606'), limit=1):
     """Query for accession numbers using UniProt REST api
@@ -13,10 +16,13 @@ def _query_uniprot(text, organisms=('9606'), limit=1):
       https://www.uniprot.org/help/uniprotkb_column_names
       https://www.uniprot.org/help/programmatic_access
 
-    :param text: gene or gene product name, synonym, id
-    :param organisms: names or taxonomy ids; can be number, string or tuple
-    :param limit: max no. hits (result rows ordered by relevance)
-    :return: xrefs - list of dictionaries [{'col1':'val1', 'col2':'val2',..},..]
+     Args:
+        text: Gene or gene product name, synonym, or id.
+        organisms: Names or taxonomy ids; can be number, string or tupleself.
+        limit: Max number of hits (result rows ordered by relevance).
+
+    Returns:
+        xrefs, a list of dictionaries list of dictionaries [{'col1':'val1', 'col2':'val2',..},..]
     """
     if len(text) < 2:
         logging.error('query text must be at least two characters long')
@@ -26,32 +32,32 @@ def _query_uniprot(text, organisms=('9606'), limit=1):
     # fields, columns can be parameters too if we'd generalize later
     fields = ('name', 'gene_exact', 'mnemonic')
     columns = ('id', 'organism-id', 'genes(PREFERRED)')
-    params = { "sort": "score", "format": "tab" }
+    params = {"sort": "score", "format": "tab"}
 
-    query=text
-    if fields!=None:
-        query=" OR ".join([f + ':' + str(text) for f in fields])
-    #filter by organism
-    if organisms!=None:
-        if isinstance(organisms, str) or isinstance(organisms, int):
-            subquery="organism:"+str(organisms)
+    query = text
+    if fields is not None:
+        query = " OR ".join([f + ':' + str(text) for f in fields])
+    # filter by organism
+    if organisms is not None:
+        if isinstance(organisms, (int, str)):
+            subquery = "organism:" + str(organisms)
         else:
             subquery = " OR ".join(['organism:'+str(o) for o in organisms])
-        query="({query}) AND ({subquery})".format(query=query,subquery=subquery)
+        query = "({query}) AND ({subquery})".format(query=query, subquery=subquery)
     params.update(query=query)
 
-    #set output data columns
-    if columns!=None:
+    # set output data columns
+    if columns is not None:
         params.update(columns=",".join(columns))
 
-    #max no. result rows (hits)
+    # max no. result rows (hits)
     if limit != None:
         params.update(limit=limit)
 
     try:
-        r = requests.get('https://www.uniprot.org/uniprot/', params=params)
-        if (r.status_code == 200):
-            lines = r.text.splitlines()
+        response = requests.get('https://www.uniprot.org/uniprot/', params=params)
+        if response.status_code == 200:
+            lines = response.text.splitlines()
             if len(lines) > 1:
                 col_names = lines[0].split('\t')
             for line in lines[1:]:
@@ -59,23 +65,24 @@ def _query_uniprot(text, organisms=('9606'), limit=1):
                 xref = dict(zip(col_names, col_vals))
                 xrefs.append(xref)
         else:
-            logging.error('Uniprot returned: {c}, params: {q}'.format(c=r.status_code, q=str(params)))
-    except requests.exceptions.RequestException as e:
-        logging.error(e)
+            LOGGER.error('Uniprot returned: %i, params: %s', response.status_code, str(params))
+    except requests.exceptions.RequestException as err:
+        logging.error(err)
 
     return xrefs
 
-
-# TODO: find and insert 'xrefs' into the annotation doc (dict)
 def ground(annotation, organisms=(9606), limit=10):
-    copy = {k:v for k,v in annotation.items()}
+    """
+    """
+    copy = {k:v for k, v in annotation.items()}
     for ent in copy['ents']:
         if ent['label'] == 'PRGE':
             xrefs = _query_uniprot(ent['text'], organisms, limit)
             ent.update(xrefs=xrefs)
     return copy
 
-
 # TODO try with HGNC rest api
 def _query_hgnc(text):
+    """
+    """
     return None
