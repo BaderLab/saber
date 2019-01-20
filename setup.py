a53fd1d519fc487da8db1989da5a68c645b7e69e
Charles Tapley Hoyt <cthoyt@gmail.com>
2019-01-20 14:42:25 +0100

    Fix requirements
    Former-commit-id: 67cccb30891615a786c0f125d3404b1cbd5406e6

--- setup.py
+++ setup.py
@@ -32,6 +32,8 @@ setuptools.setup(
         'spacy>=2.0.11, <=2.0.13',
         'gensim>=3.4.0',
         'nltk>=3.3',
+        'google-compute-engine',
+        'msgpack==0.5.6',
         'keras-contrib @ git+https://www.github.com/keras-team/keras-contrib.git',
         'en-coref-md @ https://github.com/huggingface/neuralcoref-models/releases/download/en_coref_md-3.0.0/en_coref_md-3.0.0.tar.gz',
     ],
