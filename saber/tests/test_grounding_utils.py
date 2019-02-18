13e44bf290a1dd9336399af0adc27e4ba1e67c87
johngiorgi <johnmgiorgi@gmail.com>
2019-02-17 19:07:48 -0500

    :recycle: Group requests to EXTRACT to speed up grounding
    Former-commit-id: 726d51b75d0edbc8e3361c35ac87a9e12a6f45b3
    Former-commit-id: a8f5d6298e5e1280dc7023b2664e04260d7ac549 [formerly 57bd36749eff2adcee7cd34853351019dffb502d] [formerly dde1fbd539958a3c71f8b229c64596736fab5a31 [formerly d66db436cd6c9e63a7f3d32bb8be2ef68e79518b [formerly a73257db77c0dc2090ff70102ef1c016ad4080ef]]]
    Former-commit-id: a80afd651b9e1624fc5559c9d66edf8234d4029c [formerly 1e2bd8e630c33fce026a6e3cd005265d7cb6113e]
    Former-commit-id: ac1a6632db0867cbaf34b3fcea510ed8c7fa9100
    Former-commit-id: 68a6281a77f400ccc68f6dd4322b42dd662b0f91

--- saber/tests/test_grounding_utils.py
+++ saber/tests/test_grounding_utils.py
@@ -5,53 +5,83 @@ import pytest
 from ..utils import grounding_utils
 
 @pytest.fixture
-def ched_annotation():
+def blank_annotation():
+    """Returns an annotation with no identified entities.
     """
-    """
-    annotation = {"ents": [{"text": "glucose", "label": "CHED", "start": 0, "end": 6}],
-                  "text": "glucose",
+    annotation = {"ents": [],
+                  "text": "This is a test with no entities.",
                   "title": ""}
+    return annotation
+
+@pytest.fixture
+def ched_annotation():
+    """Returns an annotation with chemical entities (CHED) identified.
+    """
+    annotation = {"ents": [{"text": "glucose", "label": "CHED", "start": 0, "end": 0},
+                           {"text": "fructose", "label": "CHED", "start": 0, "end": 0}],
+                  "text": "glucose and fructose",
+                  "title": ""}
+
     return annotation
 
 @pytest.fixture
 def diso_annotation():
+    """Returns an annotation with disease entities (DISO) identified.
     """
-    """
-    annotation = {"ents": [{"text": "cancer", "label": "DISO", "start": 0, "end": 5}],
-                  "text": "cancer",
+    annotation = {"ents": [{"text": "cancer", "label": "DISO", "start": 0, "end": 0},
+                           {"text": "cystic fibrosis", "label": "DISO", "start": 0, "end": 0}],
+                  "text": "cancer and cystic fibrosis",
                   "title": ""}
+
     return annotation
 
 @pytest.fixture
 def livb_annotation():
+    """Returns an annotation with species entities (LIVB) identified.
     """
-    """
-    annotation = {"ents": [{"text": "mouse", "label": "LIVB", "start": 0, "end": 4}],
-                  "text": "mouse",
+    annotation = {"ents": [{"text": "mouse", "label": "LIVB", "start": 0, "end": 0},
+                           {"text": "human", "label": "LIVB", "start": 0, "end": 0}],
+                  "text": "mouse and human",
                   "title": ""}
+
     return annotation
 
 @pytest.fixture
 def prge_annotation():
+    """Returns an annotation with protein/gene entities (PRGE) identified.
     """
-    """
-    annotation = {"ents": [{"text": "p53", "label": "PRGE", "start": 0, "end": 3}],
-                  "text": "p53",
+    annotation = {"ents": [{"text": "p53", "label": "PRGE", "start": 0, "end": 0},
+                           {"text": "MK2", "label": "PRGE", "start": 0, "end": 0}],
+                  "text": "p53 and MK2",
                   "title": ""}
+
     return annotation
+
+def test_ground_no_entites(blank_annotation):
+    """Asserts that `grounding_utils.ground()` returns the expected value for a simple example with
+    no identified entities.
+    """
+
+    actual = grounding_utils.ground(blank_annotation)
+    expected = blank_annotation
+
+    assert actual == expected
 
 def test_ground_chemicals(ched_annotation):
     """Asserts that `grounding_utils.ground()` returns the expected value for a simple example with
     chemical entities.
     """
-    xrefs = [
+    glucose_xrefs = [
         {'namespace': 'TODO', 'id': 'CIDs00005793'},
         {'namespace': 'TODO', 'id': 'CIDs10954115'},
         {'namespace': 'TODO', 'id': 'CIDs53782692'},
     ]
+    fructose_xrefs = [{'namespace': 'TODO', 'id': 'CIDs00439709'}]
+
+    ched_annotation['ents'][0].update(xrefs=glucose_xrefs)
+    ched_annotation['ents'][1].update(xrefs=fructose_xrefs)
 
     actual = grounding_utils.ground(ched_annotation)
-    ched_annotation['ents'][0].update(xrefs=xrefs)
     expected = ched_annotation
 
     assert actual == expected
@@ -60,12 +90,13 @@ def test_ground_diso(diso_annotation):
     """Asserts that `grounding_utils.ground()` returns the expected value for a simple example with
     disease entities.
     """
-    xrefs = [
-        {'namespace': 'TODO', 'id': 'DOID:162'},
-    ]
+    cancer_xrefs = [{'namespace': 'TODO', 'id': 'DOID:162'}]
+    cystic_fibrosis_xrefs = [{'namespace': 'TODO', 'id': 'DOID:1485'}]
+
+    diso_annotation['ents'][0].update(xrefs=cancer_xrefs)
+    diso_annotation['ents'][1].update(xrefs=cystic_fibrosis_xrefs)
 
     actual = grounding_utils.ground(diso_annotation)
-    diso_annotation['ents'][0].update(xrefs=xrefs)
     expected = diso_annotation
 
     assert actual == expected
@@ -74,13 +105,16 @@ def test_ground_livb(livb_annotation):
     """Asserts that `grounding_utils.ground()` returns the expected value for a simple example with
     species entities.
     """
-    xrefs = [
+    mouse_xrefs = [
         {'namespace': 'TODO', 'id': '10090'},
         {'namespace': 'TODO', 'id': '10088'},
     ]
+    human_xrefs = [{'namespace': 'TODO', 'id': '9606'}]
+
+    livb_annotation['ents'][0].update(xrefs=mouse_xrefs)
+    livb_annotation['ents'][1].update(xrefs=human_xrefs)
 
     actual = grounding_utils.ground(livb_annotation)
-    livb_annotation['ents'][0].update(xrefs=xrefs)
     expected = livb_annotation
 
     assert actual == expected
@@ -89,13 +123,16 @@ def test_ground_prge(prge_annotation):
     """Asserts that `grounding_utils.ground()` returns the expected value for a simple example with
     species entities.
     """
-    xrefs = [
-        {'namespace': 'TODO', 'id': 'ENSP00000269305', 'organism-id': '9606'},
-        {'namespace': 'TODO', 'id': '10088'},
+    p53_xrefs = [{'namespace': 'TODO', 'id': 'ENSP00000269305', 'organism-id': '9606'}]
+    mk2_xrefs = [
+        {'namespace': 'TODO', 'id': 'ENSP00000356070', 'organism-id': '9606'},
+        {'namespace': 'TODO', 'id': 'ENSP00000433109', 'organism-id': '9606'},
     ]
 
+    prge_annotation['ents'][0].update(xrefs=p53_xrefs)
+    prge_annotation['ents'][1].update(xrefs=mk2_xrefs)
+
     actual = grounding_utils.ground(prge_annotation)
-    prge_annotation['ents'][0].update(xrefs=xrefs)
     expected = prge_annotation
 
     assert actual == expected
