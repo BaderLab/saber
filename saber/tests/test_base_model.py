11f83baf45479eef0df252b2bfb4e8da7d4d1015
johngiorgi <johnmgiorgi@gmail.com>
2019-05-23 08:23:58 -0400

    :white_check_mark: Increase coverage for BaseModel
    Former-commit-id: ae91b436cc7ae169c17c49dadd8a81964eac15dd
    Former-commit-id: 7e2122124693646a662f3ebc3a9e0192511f8ced [formerly 9076aa1b88d19052bbb0e07d6aade56020d4c647] [formerly 4baa1adf13055b33d8af84a1be935f4c885a3f17 [formerly 7efce93b4fb1fe41572fe6176cd7cc15e2b94309 [formerly 7265e0df7d5310e089f6e57c2785cdaf35555256]]]
    Former-commit-id: 3d23488e795d5b53b8227992d4c90d9548a94eb9 [formerly 8e11e87fd5a80d4a8f97fa16a00332bcc73f9b65]
    Former-commit-id: d7b4e2eab4975e9d60050fe11f2e00cf6e88c567
    Former-commit-id: 6276eb78e7706d5fd69e559bdb542d9465c8a3a8

--- saber/tests/test_base_model.py
+++ saber/tests/test_base_model.py
@@ -1,14 +1,63 @@
 """Any and all unit tests for the BaseKerasModel (saber/models/base_model.py).
 """
+from ..models.base_model import BaseModel
 from ..models.base_model import BaseKerasModel
+from ..models.base_model import BasePyTorchModel
 from .resources.constants import *
+from .. import constants
 
 
-def test_attributes_init_of_single_model(dummy_config, dummy_dataset_1, single_base_keras_model):
-    """Asserts instance attributes are initialized correctly when single `MultiTaskLSTMCRF` model is
+def test_attributes_init_of_single_base_model(dummy_config, dummy_dataset_1, single_base_model):
+    """Asserts instance attributes are initialized correctly when single `BaseModel` model is
+    initialized.
+    """
+    assert isinstance(single_base_model, BaseModel)
+
+    # attributes that are passed to __init__
+    assert single_base_model.config is dummy_config
+    assert single_base_model.datasets[0] is dummy_dataset_1
+    assert single_base_model.embeddings is None
+
+    # other instance attributes
+    assert single_base_model.model is None
+
+    # test that we can pass arbitrary keyword arguments
+    assert single_base_model.totally_arbitrary == 'arbitrary'
+
+
+def test_attributes_init_of_compound_base_model(dummy_config,
+                                                dummy_dataset_1,
+                                                dummy_dataset_2,
+                                                compound_base_model):
+    """Asserts instance attributes are initialized correctly when compound `BaseModel` model is
+    initialized.
+    """
+    assert isinstance(compound_base_model, BaseModel)
+
+    # attributes that are passed to __init__
+    assert compound_base_model.config is dummy_config
+    assert compound_base_model.datasets[0] is dummy_dataset_1
+    assert compound_base_model.datasets[1] is dummy_dataset_2
+    assert compound_base_model.embeddings is None
+
+    # other instance attributes
+    assert compound_base_model.model is None
+
+    # test that we can pass arbitrary keyword arguments
+    assert compound_base_model.totally_arbitrary == 'arbitrary'
+
+
+def test_reset_model(single_base_model):
+    pass
+
+
+def test_attributes_init_of_single_base_keras_model(dummy_config,
+                                                    dummy_dataset_1,
+                                                    single_base_keras_model):
+    """Asserts instance attributes are initialized correctly when single `BaseKerasModel` model is
     initialized without embeddings (`embeddings` attribute is None.)
     """
-    assert isinstance(single_base_keras_model, BaseKerasModel)
+    assert isinstance(single_base_keras_model, (BaseModel, BaseKerasModel))
 
     # attributes that are passed to __init__
     assert single_base_keras_model.config is dummy_config
@@ -17,17 +66,43 @@ def test_attributes_init_of_single_model(dummy_config, dummy_dataset_1, single_b
 
     # other instance attributes
     assert single_base_keras_model.model is None
+    assert single_base_keras_model.framework == constants.KERAS
 
     # test that we can pass arbitrary keyword arguments
     assert single_base_keras_model.totally_arbitrary == 'arbitrary'
 
 
-def test_attributes_init_of_single_model_embeddings(dummy_config, dummy_dataset_1, dummy_embeddings,
+def test_attributes_init_of_compound_base_keras_model(dummy_config,
+                                                      dummy_dataset_1,
+                                                      dummy_dataset_2,
+                                                      compound_base_keras_model):
+    """Asserts instance attributes are initialized correctly when compound `BaseKerasModel` model is
+    initialized without embeddings (`embeddings` attribute is None.)
+    """
+    assert isinstance(compound_base_keras_model, (BaseModel, BaseKerasModel))
+
+    # attributes that are passed to __init__
+    assert compound_base_keras_model.config is dummy_config
+    assert compound_base_keras_model.datasets[0] is dummy_dataset_1
+    assert compound_base_keras_model.datasets[1] is dummy_dataset_2
+    assert compound_base_keras_model.embeddings is None
+
+    # other instance attributes
+    assert compound_base_keras_model.model is None
+    assert compound_base_keras_model.framework == constants.KERAS
+
+    # test that we can pass arbitrary keyword arguments
+    assert compound_base_keras_model.totally_arbitrary == 'arbitrary'
+
+
+def test_attributes_init_of_single_model_embeddings(dummy_config,
+                                                    dummy_dataset_1,
+                                                    dummy_embeddings,
                                                     single_base_keras_model_embeddings):
-    """Asserts instance attributes are initialized correctly when single `MultiTaskLSTMCRF` model is
+    """Asserts instance attributes are initialized correctly when single `BaseKerasModel` model is
     initialized with embeddings (`embeddings` attribute is not None.)
     """
-    assert isinstance(single_base_keras_model_embeddings, BaseKerasModel)
+    assert isinstance(single_base_keras_model_embeddings, (BaseModel, BaseKerasModel))
 
     # attributes that are passed to __init__
     assert single_base_keras_model_embeddings.config is dummy_config
@@ -36,6 +111,51 @@ def test_attributes_init_of_single_model_embeddings(dummy_config, dummy_dataset_
 
     # other instance attributes
     assert single_base_keras_model_embeddings.model is None
+    assert single_base_keras_model_embeddings.framework == constants.KERAS
 
     # test that we can pass arbitrary keyword arguments
     assert single_base_keras_model_embeddings.totally_arbitrary == 'arbitrary'
+
+
+def test_attributes_init_of_single_base_pytorch_model(dummy_config,
+                                                      dummy_dataset_1,
+                                                      single_base_pytorch_model):
+    """Asserts instance attributes are initialized correctly when single `BasePyTorchModel` model is
+    initialized.
+    """
+    assert isinstance(single_base_pytorch_model, (BaseModel, BasePyTorchModel))
+
+    # attributes that are passed to __init__
+    assert single_base_pytorch_model.config is dummy_config
+    assert single_base_pytorch_model.datasets[0] is dummy_dataset_1
+    assert single_base_pytorch_model.embeddings is None
+
+    # other instance attributes
+    assert single_base_pytorch_model.model is None
+    assert single_base_pytorch_model.framework == constants.PYTORCH
+
+    # test that we can pass arbitrary keyword arguments
+    assert single_base_pytorch_model.totally_arbitrary == 'arbitrary'
+
+
+def test_attributes_init_of_compound_base_pytorch_model(dummy_config,
+                                                        dummy_dataset_1,
+                                                        dummy_dataset_2,
+                                                        compound_base_pytorch_model):
+    """Asserts instance attributes are initialized correctly when single `BasePyTorchModel` model is
+    initialized.
+    """
+    assert isinstance(compound_base_pytorch_model, (BaseModel, BasePyTorchModel))
+
+    # attributes that are passed to __init__
+    assert compound_base_pytorch_model.config is dummy_config
+    assert compound_base_pytorch_model.datasets[0] is dummy_dataset_1
+    assert compound_base_pytorch_model.datasets[1] is dummy_dataset_2
+    assert compound_base_pytorch_model.embeddings is None
+
+    # other instance attributes
+    assert compound_base_pytorch_model.model is None
+    assert compound_base_pytorch_model.framework == constants.PYTORCH
+
+    # test that we can pass arbitrary keyword arguments
+    assert compound_base_pytorch_model.totally_arbitrary == 'arbitrary'
