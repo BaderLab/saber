088f2dfd3c82e2b6e3054dcec607046f747d5c71
johngiorgi <johnmgiorgi@gmail.com>
2019-07-23 20:18:23 -0400

    :art: Update structure of training data dicts
    Former-commit-id: 7083d9e988f3da7521db95e81f5218aa1ed4bf81
    Former-commit-id: 0f024bb6710c797507ad51b6ab5ab2d493a093c3 [formerly cbf9febc53efc7b776fd3409f1065e3be69bb183] [formerly 5d381709a084d107484437a2062602aa1d290184 [formerly 1fed2db9b4bded2c97b926ab31b6ff8f6cec613a [formerly 9833483f72a168c77394110e215e90b85d629e41]]]
    Former-commit-id: 6c338b6712ade418c291ee7ddf1b349f9970cd10 [formerly 7f3a26903fb34902317b86ced51e78d9289446fd]
    Former-commit-id: 2ca6ed2e16c649d61b0ab882bbaf8d91a6d188bf
    Former-commit-id: 8935c937943815e70e043cdeec3994ea0161dbed

--- saber/utils/bert_utils.py
+++ saber/utils/bert_utils.py
@@ -1,17 +1,9 @@
-import logging
-
 import torch
 from keras.preprocessing.sequence import pad_sequences
 from pytorch_pretrained_bert.optimization import BertAdam
 from torch.utils import data
 
 from saber import constants
-from saber.constants import CLS
-from saber.constants import SEP
-from saber.constants import TOK_MAP_PAD
-from saber.constants import WORDPIECE
-
-LOGGER = logging.getLogger(__name__)
 
 # TODO (johnmgiorgi): This should be handeled better. Maybe as a config argument.
 FULL_FINETUNING = True
@@ -46,16 +38,16 @@ def process_dataset_for_bert(dataset, tokenizer):
     """Process a Saber dataset to be compatible with a BERT-based model.
 
     Args:
-        dataset (Dataset): A Dataset object for which `Dataset.load()` has been called.
+        dataset (Dataset): A `Dataset` object for which `Dataset.load()` has been called.
         tokenizer (BertTokenizer): An object with methods for tokenizing text for input to BERT.
 
     Returns
-        A dictionary, keyed by '<partition_x>' and '<partition_y>' representing the processed
+        A dictionary, keyed by '[partition][x]' and '[partition][y]' representing the processed
         input and target sequences for a given partition in `dataset`.
     """
     # Required for dataset to be compatible with BERTs WordPiece tokenization
-    if WORDPIECE not in dataset.type_to_idx['ent']:
-        dataset.type_to_idx['ent'][WORDPIECE] = len(dataset.type_to_idx['ent'])
+    if constants.WORDPIECE not in dataset.type_to_idx['ent']:
+        dataset.type_to_idx['ent'][constants.WORDPIECE] = len(dataset.type_to_idx['ent'])
         dataset.get_idx_to_tag()
 
     processed_dataset = {}
@@ -76,24 +68,22 @@ def process_dataset_for_bert(dataset, tokenizer):
                                            labels=bert_labels,
                                            tag_to_idx=dataset.type_to_idx['ent'])
 
-            processed_dataset[f'x_{partition}'] = [indexed_tokens, attention_masks]
-            processed_dataset[f'y_{partition}'] = indexed_labels
-            processed_dataset[f'orig_to_tok_map_{partition}'] = orig_to_tok_map
+            processed_dataset[partition] = {
+                'x': [indexed_tokens, attention_masks], 'y': indexed_labels,
+                'orig_to_tok_map': orig_to_tok_map
+            }
+
             # TODO (John): This is just a stand-in so we can get training on BioNLP ST 2019 ASAP.
             # The process for loading relation data will need to be re-thought.
             if 'rel' in dataset.idx_seq[partition]:
-                processed_dataset[f'rel_labels_{partition}'] = dataset.idx_seq[partition]['rel']
-        # TODO (John): Do we need to make these all None?
+                processed_dataset[partition]['rel_labels'] = dataset.idx_seq[partition]['rel']
         else:
-            processed_dataset[f'x_{partition}'] = None
-            processed_dataset[f'y_{partition}'] = None
-            processed_dataset[f'orig_to_tok_map_{partition}'] = None
-            processed_dataset[f'rel_labels_{partition}'] = None
+            processed_dataset[partition] = None
 
     return processed_dataset
 
 
-# TODO (John): This does not work properly. The [SEP] tag can get dropped after padding.
+# TODO (John): This does not work properly. The [constants.SEP] tag can get dropped after padding.
 def wordpiece_tokenize_sents(tokens, tokenizer, labels=None):
     """Tokenizes pre-tokenized text for use with a BERT-based model.
 
@@ -125,12 +115,12 @@ def wordpiece_tokenize_sents(tokens, tokenizer, labels=None):
     orig_to_tok_map = []
 
     for sent in tokens:
-        bert_tokens.append([CLS])
+        bert_tokens.append([constants.CLS])
         orig_to_tok_map.append([])
         for orig_token in sent:
             orig_to_tok_map[-1].append(len(bert_tokens[-1]))
             bert_tokens[-1].extend(tokenizer.wordpiece_tokenizer.tokenize(orig_token))
-        bert_tokens[-1].append(SEP)
+        bert_tokens[-1].append(constants.SEP)
 
     # If labels are provided, project them onto bert_tokens
     if labels is not None:
@@ -139,7 +129,8 @@ def wordpiece_tokenize_sents(tokens, tokenizer, labels=None):
             labs_iter = iter(labs)
             bert_labels.append([])
             for i, _ in enumerate(bert_toks):
-                bert_labels[-1].extend([WORDPIECE if i not in tok_map else next(labs_iter)])
+                bert_labels[-1].extend([constants.WORDPIECE if i not in tok_map
+                                        else next(labs_iter)])
 
         return bert_tokens, orig_to_tok_map, bert_labels
 
@@ -147,14 +138,13 @@ def wordpiece_tokenize_sents(tokens, tokenizer, labels=None):
 
 
 def index_pad_mask_bert_tokens(tokens, orig_to_tok_map, tokenizer, labels=None, tag_to_idx=None):
-    """Convert `tokens` to indices, pad them, and generate the corresponding attention masks.
-
-    Converts a list of tokenized sentences to indicies using the given `BertTokenizer`, pads this
-    sequence and generates the corresponding attention masks.
+    """Convert `tokens`, `orig_to_tok_map` to indices, pads them, and generates the corresponding
+    attention masks.
 
     Args:
         tokens (list): A list of lists containing tokenized sentences.
-        orig_to_tok_map (TODO).
+        orig_to_tok_map (list). A list of list mapping token indices of pre-bert-tokenized text to
+            token indices in post-bert-tokenized text.
         tokenizer (BertTokenizer): An object with methods for tokenizing text for input to BERT.
         labels (list): A list of lists containing token-level labels for a collection of sentences.
         tag_to_idx (dictionary): A dictionary mapping token-level tags/labels to unique integers.
@@ -185,7 +175,7 @@ def index_pad_mask_bert_tokens(tokens, orig_to_tok_map, tokenizer, labels=None, 
         dtype='long',
         padding='post',
         truncating='post',
-        value=TOK_MAP_PAD)
+        value=constants.TOK_MAP_PAD)
     orig_to_tok_map = torch.as_tensor(orig_to_tok_map)
 
     # Generate attention masks for pad values
@@ -207,7 +197,7 @@ def index_pad_mask_bert_tokens(tokens, orig_to_tok_map, tokenizer, labels=None, 
     return indexed_tokens, orig_to_tok_map, attention_masks
 
 
-def get_dataloader_for_bert(processed_dataset, batch_size, model_idx=0):
+def get_dataloader_for_bert(processed_dataset, batch_size, model_idx=-1):
     """Returns a `DataLoader` for the given `processed_dataset`,
 
     For the given `processed_dataset` (see `bert_utils.process_dataset_for_bert`) returns a
@@ -216,27 +206,22 @@ def get_dataloader_for_bert(processed_dataset, batch_size, model_idx=0):
     Args:
         processed_dataset (dict): A dictionary containing a dataset processed for use with a
             BERT-based model. See `bert_utils.process_dataset_for_bert`.
-        attention_mask (numpy.ndarray): Numpy array or list containing the corresponding
-            attention masks labels for `x`.
         batch_size (int): Batch size of the returned `DataLoader`'s.
         model_idx (int): Optional, an integer representing the model index the returned DataLoaders
-            will correspond to. A tensor will be added to the `DataLoader`'s containing this
-            integer, expanded with `torch.expand` to match the number of input examples. This is
-            to be used in the case of multi-task models. Defaults to 0.
-
-    Raises:
-        ValueError if `data_partition` is not `train` or `eval`.
+            will correspond to. This is to be used in the case of multi-task models. Defaults to -1,
+            corresponding to the last loaded dataset.
 
     Returns:
-        A Torch DataLoader object for `x`, `y`, and `attention_masks`.
+        A dictionary of PyTorch DataLoader objects. The returned dictionary will be `None` for
+        key `partition` if `processed_dataset[partition] is None`.
     """
     dataloaders = {}
 
     for partition in constants.PARTITIONS:
-        if processed_dataset[f'x_{partition}'] is not None:
-            x, attention_mask = processed_dataset[f'x_{partition}']
-            y = processed_dataset[f'y_{partition}']
-            orig_to_tok_map = processed_dataset[f'orig_to_tok_map_{partition}']
+        if processed_dataset[partition] is not None:
+            x, attention_mask = processed_dataset[partition]['x']
+            y = processed_dataset[partition]['y']
+            orig_to_tok_map = processed_dataset[partition]['orig_to_tok_map']
 
             dataset = BertDataset(x, attention_mask, y, orig_to_tok_map, model_idx)
 
@@ -245,11 +230,11 @@ def get_dataloader_for_bert(processed_dataset, batch_size, model_idx=0):
             else:
                 sampler = data.SequentialSampler(dataset)
 
-            dataloaders[f'dataloader_{partition}'] = data.DataLoader(dataset=dataset,
-                                                                     sampler=sampler,
-                                                                     batch_size=batch_size)
+            dataloaders[partition] = data.DataLoader(dataset,
+                                                     sampler=sampler,
+                                                     batch_size=batch_size)
         else:
-            dataloaders[f'dataloader_{partition}'] = None
+            dataloaders[partition] = None
 
     return dataloaders
 
